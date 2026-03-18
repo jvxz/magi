6433c997da2303fc28cd9e7f1523e961fe57d17c
jamie <hi@jamie.to>
2026-03-18 16:56:24 -0400

    refactor: edit link in public rooms aside component

--- app/components/layout/app/aside/public-rooms.vue
+++ app/components/layout/app/aside/public-rooms.vue
@@ -1,5 +1,10 @@
 <template>
-  <NuxtLink to="/app/public">
+  <NuxtLink
+    :to="{
+      name: 'explore',
+      params: { baseUrl: 'matrix.org' },
+    }"
+  >
     <button class="rounded-xl bg-muted flex size-12 items-center justify-center">
       <Icon name="mingcute:home-3-fill" class="text-foreground size-6" />
     </button>
