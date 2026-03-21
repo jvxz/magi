788887680e855c1979522f1a748d976e6d4d30b2
jamie <hi@jamie.to>
2026-03-21 15:20:43 -0400

    feat: update app aside to include logout button

--- app/components/layout/app/aside.vue
+++ app/components/layout/app/aside.vue
@@ -1,5 +1,7 @@
 <template>
   <aside class="p-4 border-r flex flex-col gap-4 h-screen items-center top-0 sticky">
     <LayoutAppAsidePublicRooms />
+    <div class="grow" />
+    <LayoutAppAsideLogout />
   </aside>
 </template>
