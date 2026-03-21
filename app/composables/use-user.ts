e6093229e0e890c0ea9f0b259d62af24846cb1aa
jamie <hi@jamie.to>
2026-03-21 18:10:59 -0400

    feat: add userInfo to useUser composable + use getCachedData in getUserAvatar

--- app/composables/use-user.ts
+++ app/composables/use-user.ts
@@ -1,8 +1,19 @@
 export function useUser() {
   const clientStore = useClientStore()
+  const nuxtApp = useNuxtApp()
 
-  const getUserAvatar = (userId: MaybeRefOrGetter<'self' | string & {}>) => useAsyncData(`userAvatar:${userId}`, async () => {
+  const { data: userInfo } = useAsyncData(() => `userInfo:${clientStore.client.getUserId()}`, async () => {
+    const id = clientStore.client.getUserId()!
+    return clientStore.client.getUser(id)
+  }, {
+    getCachedData: key => getClientData(key, nuxtApp),
+    immediate: true,
+    watch: [() => clientStore.client.getUserId()],
+  })
+
+  const getUserAvatar = (userId: MaybeRefOrGetter<'self' | string & {}>) => useAsyncData(() => `userAvatar:${userId}`, async () => {
     const id = userId === 'self' ? clientStore.client.getUserId()! : toValue(userId)
+
     const { avatar_url } = await clientStore.client.getProfileInfo(id)
     if (!avatar_url)
       return
@@ -23,9 +34,14 @@ export function useUser() {
     const res = await fetchAuthed(url, clientStore.client, { rawResponseBody: true })
 
     return URL.createObjectURL(res)
+  }, {
+    getCachedData: key => getClientData(key, nuxtApp),
+    immediate: true,
+    watch: [() => toValue(userId)],
   })
 
   return {
     getUserAvatar,
+    userInfo,
   }
 }
