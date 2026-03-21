1a4167cf2fac56f0148fd7e37001b389b1b06789
jamie <hi@jamie.to>
2026-03-21 18:19:37 -0400

    feat: add user display name + presence to user card

--- app/components/layout/app/user-card.vue
+++ app/components/layout/app/user-card.vue
@@ -1,15 +1,25 @@
 <script lang="ts" setup>
-const { getUserAvatar } = useUser()
+const { getUserAvatar, userInfo } = useUser()
 
 const { data: avatarUrl, pending } = getUserAvatar('self')
 </script>
 
 <template>
   <div class="p-3 h-21 w-full bottom-0 left-0 absolute">
-    <div class="px-4 border rounded bg-card-2 flex size-full items-center">
-      <UAvatar class="size-8" :class="pending && 'animate-pulse'">
-        <UAvatarImage v-if="avatarUrl" :src="avatarUrl" />
-      </UAvatar>
+    <div class="border rounded bg-card-2 flex size-full items-center">
+      <div class="p-2 px-4 flex gap-2 h-full items-center">
+        <UAvatar class="size-8" :class="pending && 'animate-pulse'">
+          <UAvatarImage v-if="avatarUrl" :src="avatarUrl" />
+        </UAvatar>
+        <div class="flex-col h-full">
+          <p class="text-sm font-medium">
+            {{ userInfo?.displayName }}
+          </p>
+          <p class="text-2xs text-muted-foreground ">
+            {{ userInfo?.presence ? upperFirst(userInfo?.presence) : 'Offline' }}
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
