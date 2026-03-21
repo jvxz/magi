314539c4d5985b53c501f6362c8dfcfe6595323f
jamie <hi@jamie.to>
2026-03-21 18:28:34 -0400

    feat: add hover effect to user card profile section

--- app/components/layout/app/user-card.vue
+++ app/components/layout/app/user-card.vue
@@ -7,17 +7,19 @@ const { data: avatarUrl, pending } = getUserAvatar('self')
 <template>
   <div class="p-3 h-21 w-full bottom-0 left-0 absolute">
     <div class="border rounded bg-card-2 flex size-full items-center">
-      <div class="p-2 px-4 flex gap-2 h-full items-center">
-        <UAvatar class="size-8" :class="pending && 'animate-pulse'">
-          <UAvatarImage v-if="avatarUrl" :src="avatarUrl" />
-        </UAvatar>
-        <div class="flex-col h-full">
-          <p class="text-sm font-medium">
-            {{ userInfo?.displayName }}
-          </p>
-          <p class="text-2xs text-muted-foreground ">
-            {{ userInfo?.presence ? upperFirst(userInfo?.presence) : 'Offline' }}
-          </p>
+      <div class="p-3 flex gap-2 w-full items-center">
+        <div class="p-1.5 rounded-sm flex gap-2 h-full w-full cursor-pointer duration-140 items-center -m-1.5 hover:bg-white/7.5">
+          <UAvatar class="size-8" :class="pending && 'animate-pulse'">
+            <UAvatarImage v-if="avatarUrl" :src="avatarUrl" />
+          </UAvatar>
+          <div class="flex-col h-full -translate-y-0.5">
+            <p class="text-sm font-medium">
+              {{ userInfo?.displayName }}
+            </p>
+            <p class="text-2xs text-muted-foreground">
+              {{ userInfo?.presence ? upperFirst(userInfo?.presence) : 'Offline' }}
+            </p>
+          </div>
         </div>
       </div>
     </div>
