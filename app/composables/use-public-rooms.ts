7dae3d3739fc648877292914b550ecaf426554c8
jamie <hi@jamie.to>
2026-03-21 15:21:14 -0400

    refactor: edit usePublicRooms composable

--- app/composables/use-public-rooms.ts
+++ app/composables/use-public-rooms.ts
@@ -1,12 +1,14 @@
 import type { AsyncDataOptions } from '#app'
+import type { MatrixClient } from 'matrix-js-sdk'
 
 export function usePublicRooms(_server: MaybeRefOrGetter<string>) {
-  const { $matrix, ...nuxtApp } = useNuxtApp()
-  const server = computed(() => withHttps(toValue(_server ?? MATRIX_BASE_URL)))
+  const nuxtApp = useNuxtApp()
+  const clientStore = useClientStore()
+  const server = computed(() => withoutTrailingSlash(withoutProtocol(toValue(_server))))
 
-  const getPublicRooms = (opts?: AsyncDataOptions<Awaited<ReturnType<typeof $matrix.publicRooms>>>) => useAsyncData(
+  const getPublicRooms = (opts?: AsyncDataOptions<Awaited<ReturnType<MatrixClient['publicRooms']>>>) => useAsyncData(
     () => `publicRooms:${server.value}`,
-    async () => $matrix.publicRooms({ server: server.value }),
+    async () => clientStore.client.publicRooms({ server: server.value }),
     {
       ...opts,
       getCachedData: key => nuxtApp.payload.data?.[key] || nuxtApp.static?.data?.[key] || undefined,
