87fa34603398367473c2f52ef3ae32649d241ee8
jamie <hi@jamie.to>
2026-03-22 16:53:39 -0400

    refactor: extract idb unstorage def to own file

--- app/composables/use-idb.ts
+++ app/composables/use-idb.ts
@@ -1,10 +1,3 @@
-import { createStorage } from 'unstorage'
-import indexedDbDriver from 'unstorage/drivers/indexedb'
-
-const storage = createStorage({
-  driver: indexedDbDriver({ base: 'decoy' }),
-})
-
 export function useIdb() {
-  return storage
+  return idb
 }
