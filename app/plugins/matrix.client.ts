fe96b0632fb61a05f3e04f15c932bedead62ba61
jamie <hi@jamie.to>
2026-03-23 15:28:39 -0400

    refactor: rename client.client.ts to matrix.client.ts

--- app/plugins/matrix.client.ts
+++ app/plugins/matrix.client.ts
@@ -1,15 +1,17 @@
-import { createClient } from 'matrix-js-sdk'
-
 export default defineNuxtPlugin({
-  name: 'matrix',
-  parallel: true,
-  setup: () => {
-    const matrix = createClient({ baseUrl: MATRIX_BASE_URL })
+  order: 0,
+  setup: async () => {
+    const { loginPersisted } = useAuth()
+    const { client } = useMatrixClient()
 
-    return {
-      provide: {
-        matrix,
-      },
+    const persistedClient = await loginPersisted()
+
+    if (persistedClient) {
+      client.value = persistedClient
+
+      const sw = await getServiceWorker()
+      if (sw)
+        await sendSessionToSw(client.value.getHomeserverUrl(), client.value.getAccessToken() ?? undefined)
     }
   },
 })
