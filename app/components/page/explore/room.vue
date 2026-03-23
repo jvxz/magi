f5da91a1205c8f94b10365ed44cc2e1c8357c870
jamie <hi@jamie.to>
2026-03-23 15:36:42 -0400

    refactor: remove intersection + custom image loading logic from room component

--- app/components/page/explore/room.vue
+++ app/components/page/explore/room.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
 import type { IPublicRoomsChunkRoom } from 'matrix-js-sdk'
-import { Method } from 'matrix-js-sdk'
 
 const props = defineProps<{
   room: IPublicRoomsChunkRoom | undefined
@@ -21,57 +20,17 @@ const src = computed(
     }) ?? ''
     : '',
 )
-
-const isIntersecting = ref(false)
-
-const nuxtApp = useNuxtApp()
-const { data: imageData } = useAsyncData(() => `avatar:${props.room?.room_id}`, async () => {
-  try {
-    if (!isIntersecting.value)
-      return
-
-    const url = new URL(src.value)
-
-    const queryParams: Record<string, string> = {}
-    url.searchParams.forEach((v, k) => {
-      queryParams[k] = v
-    })
-
-    const blob = await client.value.http.authedRequest<Blob>(
-      Method.Get,
-      url.pathname,
-      queryParams,
-      undefined,
-      { prefix: '', rawResponseBody: true },
-    )
-
-    return URL.createObjectURL(blob)
-  }
-  catch {}
-}, {
-  getCachedData: key => nuxtApp.payload.data?.[key] || nuxtApp.static?.data?.[key] || undefined,
-  lazy: true,
-  server: false,
-  watch: [src, isIntersecting],
-})
-
-const container = useTemplateRef<HTMLDivElement>('container')
-useIntersectionObserver(container, (entries) => {
-  if (entries[0])
-    isIntersecting.value = entries[0].isIntersecting
-})
 </script>
 
 <template>
   <div
-    ref="container"
     class="rounded-lg w-full relative overflow-hidden"
     :class="!room && 'animate-pulse border'"
   >
     <div class="h-full inset-0 absolute">
       <LazyNuxtImg
-        v-if="imageData && isIntersecting"
-        :src="imageData"
+        v-if="src"
+        :src="src"
         :data-loaded="room ? 'true' : 'false'"
         class="bg-primary size-full scale-120 object-cover blur-xl -m-px data-[loaded=false]:bg-card-2 -translate-y-1/3 data-[loaded=false]:blur-none"
       />
@@ -80,7 +39,7 @@ useIntersectionObserver(container, (entries) => {
     <div class="mt-2/5 p-4 pt-1/8 border border-t-0 rounded-b-lg bg-card-2 flex flex-col gap-2 relative z-10 isolate">
       <AvatarRoot class="border-5 border-card-2 rounded-2xl bg-card-2 flex size-16 items-center inset-0 left-4 justify-center absolute z-10 overflow-hidden -top-1/8">
         <template v-if="room">
-          <AvatarImage v-if="imageData" :src="imageData" />
+          <AvatarImage v-if="src" :src="src" />
           <AvatarFallback class="text-sm text-foreground font-medium">
             {{ room.name ? room.name.slice(0, 2) : room.room_id.slice(0, 2) }}
           </AvatarFallback>
