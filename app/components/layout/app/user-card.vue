f3a0e5fb888ff82af3619b0445b86d79b07f228e
jamie <hi@jamie.to>
2026-03-24 15:08:35 -0400

    feat: finish user card mockup, adding settings + mute + deafen buttons

--- app/components/layout/app/user-card.vue
+++ app/components/layout/app/user-card.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="p-3 shrink-0 h-20 w-full -mt-3">
+  <div class="p-3 shrink-0 h-21 w-full -mt-3">
     <div class="group border rounded bg-card-2 flex size-full items-center">
-      <div class="p-3.5 flex gap-2 w-full items-center">
+      <div class="p-3.5 flex gap-4 w-full items-center">
         <UserCardProfile />
+        <UserCardActions/>
       </div>
     </div>
   </div>
---
+++ app/components/user-card/actions.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex shrink-0 gap-2 items-center">
+    <UserCardActionButton
+      button-icon="mingcute:mic-fill"
+      button-label="Mute"
+      arrow-label="Input Settings"
+    />
+    <UserCardActionButton
+      button-icon="mingcute:headphone-fill"
+      button-label="Deafen"
+      arrow-label="Output Settings"
+    />
+    <UTooltipRoot>
+      <UTooltipTrigger as-child>
+        <UButton variant="ghost" size="icon" class="group/btn">
+          <Icon name="mingcute:settings-4-fill" class="size-5 group-hover/btn:rotate-180 duration-0 motion-reduce:duration-0! group-hover/btn:duration-1100 ease" />
+        </UButton>
+      </UTooltipTrigger>
+      <UTooltipContent>
+        User Settings
+      </UTooltipContent>
+    </UTooltipRoot>
+  </div>
+</template>
