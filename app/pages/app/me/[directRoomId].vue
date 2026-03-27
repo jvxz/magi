24d0bd41aa6aa04693725a7d6f299eedd739bfcd
jamie <hi@jamie.to>
2026-03-27 15:43:12 -0400

    feat: add mockup page for displaying room info

---
+++ app/pages/app/me/[directRoomId].vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'me',
+  middleware: ({ params }) => {
+    if (('directRoomId' in params) && !params.directRoomId) {
+      return navigateTo({
+        name: 'me',
+      })
+    }
+  },
+  name: 'direct-room',
+})
+
+const route = useRoute()
+</script>
+
+<template>
+  <pre>{{ route.params.directRoomId }}</pre>
+</template>
