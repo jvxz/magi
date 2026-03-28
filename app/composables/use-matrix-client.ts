2811f68bd7d8ef95ee3fa42cb6d01a0224eca225
jamie <hi@jamie.to>
2026-03-28 19:44:36 -0400

    feat: improve useMatrixClient composable with initAuthedClient function

--- app/composables/use-matrix-client.ts
+++ app/composables/use-matrix-client.ts
@@ -1,15 +1,45 @@
 import type { MatrixClient } from 'matrix-js-sdk'
-import { createClient } from 'matrix-js-sdk'
+import { ClientEvent, createClient, SyncState } from 'matrix-js-sdk'
 
 export const useMatrixClient = createGlobalState(() => {
   const client = shallowRef<MatrixClient>(createClient({ baseUrl: MATRIX_BASE_URL }))
 
-  function reset() {
-    client.value = createClient({ baseUrl: MATRIX_BASE_URL })
+  async function initAuthedClient(waitForPrepared = true) {
+    const auth = await idb.getItem<AuthPayload>('auth')
+    if (!auth)
+      return
+
+    try {
+      const authedClient = await createAuthedClient(auth)
+
+      void startClient(authedClient)
+      if (waitForPrepared) {
+        await new Promise<void>((resolve, reject) => {
+          const onSync = (syncState: SyncState, _prevState: SyncState | null) => {
+            if (syncState === SyncState.Prepared) {
+              authedClient.off(ClientEvent.Sync, onSync)
+              resolve()
+            }
+
+            if (syncState === SyncState.Error)
+              reject(new Error('sync state was ERROR'))
+          }
+          authedClient.on(ClientEvent.Sync, onSync)
+        })
+      }
+
+      client.value = authedClient
+      await messageSw('session', auth)
+
+      return authedClient
+    }
+    catch (err) {
+      console.error('Error when initializing authed client: ', err)
+    }
   }
 
   return {
     client,
-    reset,
+    initAuthedClient,
   }
 })
