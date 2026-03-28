33484074833c6b4f6d4a8e5bfe085ba3336c31ed
jamie <hi@jamie.to>
2026-03-28 19:50:20 -0400

    feat: improve auth middleware

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -2,12 +2,11 @@ export default defineNuxtRouteMiddleware(async (to) => {
   if (import.meta.server)
     return
 
-  const { client } = useMatrixClient()
-  const unauthed = !client.value.getUserId()
+  const status = useMatrixStatus()
 
-  if (unauthed && to.path.includes('/app'))
+  if (!status.value.isAuthed && to.path.includes('/app'))
     return navigateTo('/login')
 
-  if (!unauthed && to.path.includes('/login'))
+  if (status.value.isAuthed && to.path.includes('/login'))
     return navigateTo('/app')
 })
