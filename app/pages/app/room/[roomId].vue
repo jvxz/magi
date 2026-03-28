08a00bc486493aed224ba9e8a064b7908e0f56b1
jamie <hi@jamie.to>
2026-03-28 19:51:32 -0400

    feat: add mockup room page

---
+++ app/pages/app/room/[roomId].vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'room',
+  middleware: ({ params }) => {
+    if (('roomId' in params) && !params.roomId) {
+      return navigateTo({
+        name: 'me',
+      })
+    }
+  },
+  name: 'room',
+})
+
+const route = useRoute()
+</script>
+
+<template>
+  <pre>{{ route.params.roomId }}</pre>
+</template>
