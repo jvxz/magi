29282e64f83e75f25888ea02e608585427feb21d
jamie <hi@jamie.to>
2026-03-29 17:32:37 -0400

    refactor: edit settings dialog sidebar

--- app/components/settings/dialog/sidebar.vue
+++ app/components/settings/dialog/sidebar.vue
@@ -3,7 +3,7 @@ const { me } = useUser()
 </script>
 
 <template>
-  <div class="p-4 bg-background shrink-0 w-64">
+  <div class="p-4 bg-background shrink-0 w-64 rounded-l-lg">
     <UButton variant="ghost" class="group text-foreground font-normal p-3 gap-3.5 h-[4.25rem] w-full items-center justify-between">
       <MatrixAvatar
         :user="me"
