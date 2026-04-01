9c341621cf5b74ab847c85f7b048d995c3b3387c
jamie <hi@jamie.to>
2026-04-01 19:49:23 -0400

    feat: add more hooks to useRoomEventHooks

--- app/composables/use-room-event-hooks.ts
+++ app/composables/use-room-event-hooks.ts
@@ -1,10 +1,12 @@
-import type { Listener, MatrixEvent, Room, RoomEmittedEvents, RoomEventHandlerMap, RoomMember, RoomState } from 'matrix-js-sdk'
+import type { EventTimelineSet, Listener, MatrixEvent, Room, RoomEmittedEvents, RoomEventHandlerMap, RoomMember, RoomState } from 'matrix-js-sdk'
 import { RoomEvent } from 'matrix-js-sdk'
 
 type EmitterListener<T extends RoomEmittedEvents> = Listener<RoomEmittedEvents, RoomEventHandlerMap, T>
 
 type Params = Partial<{
   onTimeline: (event: MatrixEvent) => void
+  onTimelineRefresh: (room: Room, eventTimelineSet: EventTimelineSet) => void
+  onTimelineReset: (room: Room | undefined, eventTimelineSet: EventTimelineSet, resetAllTimelines: boolean) => void
   onCurrentStateUpdated: (room: Room, previousRoomState: RoomState, roomState: RoomState) => void
   onAccountData: (event: MatrixEvent, room: Room, prevEvent?: MatrixEvent | undefined) => void
   onRoomMemberTyping: (event: MatrixEvent, member: RoomMember) => void
@@ -18,13 +20,15 @@ export function useRoomEventHooks(roomId: MaybeRefOrGetter<string | undefined>, 
   const disposers: (() => void)[] = []
   const cleanup = () => disposers.forEach(disposer => disposer())
 
-  watch([room], ([current]) => {
+  watch(room, () => {
     cleanup()
 
-    bindListener(RoomEvent.Timeline, params?.onTimeline, disposers, current)
-    bindListener(RoomEvent.CurrentStateUpdated, params?.onCurrentStateUpdated, disposers, current)
-    bindListener(RoomEvent.AccountData, params?.onAccountData, disposers, current)
-  })
+    bindListener(RoomEvent.Timeline, params?.onTimeline, disposers, room.value)
+    bindListener(RoomEvent.TimelineRefresh, params?.onTimelineRefresh, disposers, room.value)
+    bindListener(RoomEvent.TimelineReset, params?.onTimelineReset, disposers, room.value)
+    bindListener(RoomEvent.CurrentStateUpdated, params?.onCurrentStateUpdated, disposers, room.value)
+    bindListener(RoomEvent.AccountData, params?.onAccountData, disposers, room.value)
+  }, { immediate: true })
 
   params?.onRoomMemberTyping && roomMemberTypingHook.on((event, member) => {
     params?.onRoomMemberTyping?.(event, member)
