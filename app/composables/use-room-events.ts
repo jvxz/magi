403630ed5162b767920f6b479a014c237a6d23d9
jamie <hi@jamie.to>
2026-04-01 19:49:46 -0400

    feat: heavily improve useRoomEvents composable

--- app/composables/use-room-events.ts
+++ app/composables/use-room-events.ts
@@ -1,33 +1,42 @@
-export function useRoomEvents(roomId: MaybeRefOrGetter<string | undefined>) {
-  const roomIdRef = toRef(roomId)
+import type { MatrixEvent, Room } from 'matrix-js-sdk'
 
+export function useRoomEvents(room: MaybeRefOrGetter<Room | undefined>) {
+  const roomRef = toRef(room)
   const { client } = useMatrixClient()
-  const room = useRoom(roomIdRef)
 
-  const events = computedWithControl(room, () => {
-    if (!room.value)
-      return []
+  const events = shallowRef<MatrixEvent[]>(roomRef.value?.getLiveTimeline().getEvents() ?? [])
 
-    const tl = room.value.getLiveTimeline()
-    return tl.getEvents() ?? []
-  })
+  const sync = () => {
+    const liveEvents = roomRef.value?.getLiveTimeline().getEvents()
 
-  watch(events, async (e) => {
-    if (room.value && e.length < 30) {
-      const tl = room.value.getLiveTimeline()
+    events.value = [...(liveEvents ?? [])]
+  }
 
-      await client.value.paginateEventTimeline(tl, {
+  watchImmediate(events, async () => {
+    if (events.value.length < 30) {
+      const tl = roomRef.value?.getLiveTimeline()
+      if (!tl)
+        return
+
+      const res = await client.value.paginateEventTimeline(tl, {
         backwards: true,
-        limit: 30 - e.length,
+        limit: 30 - events.value.length,
       })
 
-      events.trigger()
+      if (res)
+        sync()
     }
   })
 
-  useRoomEventHooks(roomIdRef, {
-    onTimeline: () => events.trigger(),
+  useRoomEventHooks(() => roomRef.value?.roomId, {
+    onTimeline: () => sync(),
+    onTimelineRefresh: () => sync(),
+    onTimelineReset: () => sync(),
   })
+
+  // sync on decrypted events (usually messages)
+  const { onDecrypted } = useMatrixHooks()
+  onDecrypted(sync)
 
   return events
 }
