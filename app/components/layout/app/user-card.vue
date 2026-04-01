664281cdaa143f15e6b9c9e3a4dd12d503968d0d
jamie <hi@jamie.to>
2026-04-01 19:53:02 -0400

    refactor: edit LayoutAppUserCard component

--- app/components/layout/app/user-card.vue
+++ app/components/layout/app/user-card.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="p-3 shrink-0 h-21 w-full -mt-3">
+  <div class="shrink-0 h-user-card-height px-3 -translate-y-3 w-full">
     <div class="group border rounded bg-card-2 flex size-full items-center">
-      <div class="p-3.5 flex gap-4 w-full items-center">
+      <div class="px-3.5 flex gap-4 w-full items-center">
         <UserCardProfile />
         <UserCardActions />
       </div>
