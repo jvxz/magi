329b91cb0cc40a5e323f71cd77f6d494fdc32841
jamie <hi@jamie.to>
2026-04-09 15:27:45 -0400

    refactor: edit room page components

--- app/components/page/room/event/message.vue
+++ app/components/page/room/event/message.vue
@@ -1,23 +1,31 @@
 <script lang="ts" setup>
 import type { MatrixEvent } from 'matrix-js-sdk'
-import { EventType } from 'matrix-js-sdk'
 
 const props = defineProps<{
-  content: MatrixEvent
+  event: MatrixEvent
 }>()
-
-assert(props.content.getType() === EventType.RoomMessage, 'Event provided in PageRoomEventMessage is not a RoomMessage event')
 
 const { client } = useMatrixClient()
 
-const member = getMember(client.value, props.content.getSender(), props.content.getRoomId())
-const content = props.content.getContent()
+const member = getMember(client.value, props.event.getSender(), props.event.getRoomId())
+const content = props.event.getContent()
 
-const displayName = member?.displayName ?? props.content.getSender()
+const displayName = member?.displayName ?? props.event.getSender()
+
+const isEncrypted = computed(() => {
+  if (!props.event.isEncrypted())
+    return false
+
+  return props.event.getClearContent() === null
+})
 </script>
 
 <template>
-  <PageRoomEvent data-event-type="message" class="flex gap-4 select-text">
+  <PageRoomEvent
+    :event-id="props.event.getId()"
+    :event-type="props.event.getType()"
+    class="flex gap-4 select-text"
+  >
     <MatrixAvatar
       :user="member"
       :size="32"
@@ -28,15 +36,18 @@ const displayName = member?.displayName ?? props.content.getSender()
       <p class="font-medium space-x-2">
         <span class="text-sm">{{ displayName }}</span>
         <NuxtTime
-          :datetime="props.content.getTs()"
+          :datetime="props.event.getTs()"
           hour="numeric"
           minute="numeric"
 
           class="text-xs text-muted-foreground"
         />
       </p>
-      <p class="">
+      <p v-if="!isEncrypted" class="">
         {{ content.body }}
+      </p>
+      <p v-else class="">
+        Encrypted message
       </p>
     </div>
   </PageRoomEvent>
