5e9b853a0935ff827b7722ea2e71da4b4832fc7a
jamie <hi@jamie.to>
2026-04-10 13:38:19 -0400

    feat: add env indicator for test + dev

--- app/components/layout/app/header.vue
+++ app/components/layout/app/header.vue
@@ -4,12 +4,29 @@ defineProps<{
 }>()
 
 const currentRoom = useCurrentRoom()
+
+const env = computed(() => {
+  if (!import.meta.dev)
+    return
+
+  if (import.meta.dev)
+    return 'Development'
+
+  if (import.meta.test)
+    return 'Testing'
+
+  return undefined
+})
 </script>
 
 <template>
-  <header class="flex shrink-0 h-app-header-height items-center justify-center">
-    <p class="text-sm font-medium">
+  <header class="flex shrink-0 h-app-header-height items-center justify-between *:shrink-0">
+    <div class="flex-1 flex items-center">
+      <span class="text-xs text-muted-foreground font-medium ml-2">{{ env }}</span>
+    </div>
+    <p class="text-sm font-medium text-center w-auto">
       {{ title ?? currentRoom?.name ?? upperFirst($route.name) }}
     </p>
+    <div class="flex-1" />
   </header>
 </template>
