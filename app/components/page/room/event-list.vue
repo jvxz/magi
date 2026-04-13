9bcc8d75e3ddbcd06e81645142da481ad9a72cab
jamie <hi@jamie.to>
2026-04-12 21:52:31 -0400

    refactor: use useEventPagination composable in event list composable

--- app/components/page/room/event-list.vue
+++ app/components/page/room/event-list.vue
@@ -1,103 +1,31 @@
 <script lang="ts" setup>
-import type { MatrixEvent, Room } from 'matrix-js-sdk'
+import type { Room } from 'matrix-js-sdk'
 import { EventType } from 'matrix-js-sdk'
 
 const props = defineProps<{
   room: Room
 }>()
 
-const {
-  events,
-  getEventVersion,
-  isFullyLoaded,
-  scrollEventsAsync,
-} = useRoomEvents(toRef(props, 'room'))
-
-const containerRef = useTemplateRef<HTMLDivElement>('container')
+const containerRef = useTemplateRef('container')
 const wrapperRef = useTemplateRef('wrapper')
 
-const { isReady, itemBind, listPaginated: eventsPaginated, loadOlder } = useScrollPagination(events, containerRef, {
-  canPaginate: ({ endItem, listPaginated }) => {
-    if (!events.value.length)
-      return false
-
-    const backward = !isFullyLoaded.value || !listPaginated.includes(events.value[0]!)
-    const forward = events.value.at(-1) !== endItem
-
-    return {
-      backward,
-      forward,
-    }
-  },
-  customItemsContainer: wrapperRef,
-  identifier: 'getId',
-  key: props.room.roomId,
-  onBeforePaginate: async ({ dir, endItem, isOnLatestPage, listPaginated, startItem }) => {
-    const chunk = await loadChunk(dir, startItem, endItem, isOnLatestPage)
-    return dir === 'backward' ? [...chunk, ...listPaginated] : [...listPaginated, ...chunk]
-  },
-  windowSize: 7000,
+const {
+  createItemBind,
+  eventsPaginated,
+  getEventVersion,
+  handleOnMounted,
+  scrollToBottom,
+} = useEventPagination({
+  itemsEl: wrapperRef,
+  room: toRef(props, 'room'),
+  scrollEl: containerRef,
 })
 
-async function loadChunk(
-  dir: 'forward' | 'backward',
-  startEvent: MatrixEvent,
-  endEvent: MatrixEvent,
-  isOnLatestPage: boolean,
-) {
-  const startId = startEvent.getId()
-  const endId = endEvent.getId()
-
-  if (!startId || !endId)
-    return []
-
-  if (dir === 'backward') {
-    const prevStartEventIdx = events.value.findIndex(event => event.getId() === startId)
-    if (prevStartEventIdx === -1)
-      return []
-
-    const remainingCachedEvents = events.value.slice(0, prevStartEventIdx)
-    if (remainingCachedEvents.length) {
-      const start = Math.max(0, prevStartEventIdx - BATCH_SIZE)
-      return events.value.slice(start, prevStartEventIdx)
-    }
-
-    const prevEventsLength = events.value.length
-
-    await scrollEventsAsync(dir)
-
-    const amountAdded = events.value.length - prevEventsLength
-
-    const startEventIdx = Math.max(0, prevStartEventIdx + amountAdded)
-
-    const chunkSize = amountAdded > 0 ? amountAdded : BATCH_SIZE
-    const start = Math.max(0, startEventIdx - chunkSize)
-
-    return events.value.slice(start, startEventIdx)
-  }
-
-  const endEventIdx = events.value.findIndex(event => event.getId() === endId)
-  if (endEventIdx === -1)
-    return []
-
-  const start = endEventIdx + (isOnLatestPage ? 1 : 0)
-  const end = Math.min(events.value.length, start + BATCH_SIZE)
-
-  return events.value.slice(start, end)
-}
-
-whenever(isReady, async () => {
-  const container = unrefElement(containerRef)
-  if (!container)
-    return
-
-  while (container.scrollHeight <= container.clientHeight && !isFullyLoaded.value) {
-    const prevHeight = container.scrollHeight
-    await loadOlder()
-    if (prevHeight === container.scrollHeight)
-      break
-  }
-}, { once: true })
+onMounted(async () => {
+  await nextTick()
+  scrollToBottom()
+  await handleOnMounted()
+})
 </script>
 
 <template>
@@ -105,12 +33,17 @@ whenever(isReady, async () => {
     ref="container"
     class="scroll-container grid h-[calc(100%-3rem)] w-full content-end absolute overflow-x-hidden overflow-y-scroll"
   >
-    <div ref="wrapper" class="w-full" data-testid="scroll-container-wrapper">
+    <div
+      ref="wrapper"
+      class="w-full"
+      data-testid="scroll-container-wrapper"
+    >
       <div
         v-for="(event, idx) in eventsPaginated"
         :key="`${event.getId() ?? idx}:${getEventVersion(event.getId() ?? '')}`"
-        class="pb-4.25"
-        v-bind="itemBind(event, idx)"
+        class="pb-4.25 where-last:pb-8"
+        :style="$config.public.testMode ? { height: `${(event as any)._size}px` } : undefined"
+        v-bind="createItemBind(event, idx)"
       >
         <PageRoomEventMessage
           v-if="event.getType() === EventType.RoomMessage || event.getType() === EventType.RoomMessageEncrypted"
@@ -121,7 +54,7 @@ whenever(isReady, async () => {
           :event
         />
       </div>
-      <div class="h-8" />
+      <div data-ignore class="h-8" />
     </div>
   </div>
 </template>
