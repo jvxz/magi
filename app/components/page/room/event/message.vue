f3986fad83b0a6d6d349977534d404bb9fdfa799
jamie <hi@jamie.to>
2026-04-18 15:10:52 -0400

    feat: enhance room message event component

--- app/components/page/room/event/message.vue
+++ app/components/page/room/event/message.vue
@@ -1,45 +1,93 @@
 <script lang="ts" setup>
-import type { MatrixEvent } from 'matrix-js-sdk'
+import type { MatrixEvent, Room } from 'matrix-js-sdk'
+import { MsgType } from 'matrix-js-sdk'
 
 const props = defineProps<{
   event: MatrixEvent
+  grouped: boolean
+  room: Room
 }>()
 
 const { client } = useMatrixClient()
 
 const member = getMember(client.value, props.event.getSender(), props.event.getRoomId())
-const content = props.event.getContent()
 
 const displayName = member?.displayName ?? props.event.getSender()
 
-const isEncrypted = computed(() => {
-  if (!props.event.isEncrypted())
-    return false
+const { data: replyEvent, isLoading: isReplyEventLoading, isReplyEvent } = useRoomReplyEvent(props.event, props.room)
 
-  return props.event.getClearContent() === null
+const isDecrypting = computed(() => props.event.isBeingDecrypted())
+
+const shouldRender = computed(() => {
+  const { event } = props
+  const content = event.getContent()
+
+  const type = content.msgtype
+
+  const isMsg = type === MsgType.Text || type === 'm.bad.encrypted'
+  const isEdit = isEditEvent(event)
+
+  return (isMsg || isDecrypting.value) && !isEdit
 })
+
+const eventContent = computed(() => getEventContent(props.event).body)
+const replyEventContent = computed(() => getEventContent(replyEvent.value).body)
 </script>
 
 <template>
   <PageRoomEvent
+    v-if="shouldRender"
     :event-id="props.event.getId()"
     :event-type="props.event.getType()"
-    as-child
+    :grouped
+    class="py-0.5"
   >
-    <PageRoomEventMessageRoot>
-      <PageRoomEventMessageAvatar :user="member" />
-      <PageRoomEventMessageContent>
-        <template #header>
-          {{ displayName }}
-          <NuxtTime
-            :datetime="event.getTs()"
-            hour="numeric"
-            minute="numeric"
-            class="text-xs text-muted-foreground"
-          />
+    <PageRoomEventMessageRoot class="flex-col gap-px">
+      <div v-if="isReplyEvent" class="text-sm flex gap-1.5 items-center relative">
+        <Icon name="custom:reply" class="text-muted-foreground shrink-0 h-6 w-12 translate-x-2.5 translate-y-1" />
+
+        <div class="ps-1.5">
+          <MatrixAvatar class="size-3.5" :user="replyEvent?.getSender()" />
+        </div>
+
+        <template v-if="!isReplyEventLoading">
+          <p class="text-muted-foreground font-medium">
+            {{ replyEvent?.getSender() }}
+          </p>
+
+          <p
+            class="max-w-2/3 truncate"
+            :class="{ 'italic text-muted-foreground': replyEvent?.isDecryptionFailure() }"
+          >
+            {{ replyEventContent }}
+          </p>
         </template>
-        <PageRoomEventMessageBody :content="content.body" :is-encrypted="isEncrypted" />
-      </PageRoomEventMessageContent>
+        <template v-else>
+          <USkeleton class="h-4 w-32" />
+        </template>
+      </div>
+
+      <div class="flex gap-4">
+        <PageRoomEventMessageAvatar :user="member?.userId" />
+        <PageRoomEventMessageContent>
+          <template v-if="!grouped" #header>
+            {{ displayName }}
+            <NuxtTime
+              :datetime="event.getTs()"
+              hour="numeric"
+              minute="numeric"
+              class="text-xs text-muted-foreground"
+            />
+          </template>
+
+          <p v-if="!isDecrypting" :class="{ 'italic text-muted-foreground': event?.isDecryptionFailure() }">
+            {{ eventContent }}
+          </p>
+          <p v-else class="italic">
+            Decrypting message...
+          </p>
+        </PageRoomEventMessageContent>
+      </div>
     </PageRoomEventMessageRoot>
   </PageRoomEvent>
 </template>
