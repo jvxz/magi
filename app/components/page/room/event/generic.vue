00cc44fd2a82af02476c74493bf56176c032507e
jamie <hi@jamie.to>
2026-04-18 15:11:02 -0400

    feat: create generic room event component

---
+++ app/components/page/room/event/generic.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import type { MatrixEvent, Room } from 'matrix-js-sdk'
+import { EventType } from 'matrix-js-sdk'
+
+const props = defineProps<{
+  event: MatrixEvent
+  grouped: boolean
+  room: Room
+}>()
+
+const type = computed(() => props.event.getType())
+</script>
+
+<template>
+  <PageRoomEventMessage
+    v-if="type === EventType.RoomMessage || EventType.RoomMessageEncrypted" 
+    :event
+    :grouped
+    :room
+  />
+  <PageRoomEventMember
+    v-else-if="type === EventType.RoomMember"
+    :event
+  />
+</template>
