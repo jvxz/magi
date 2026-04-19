933b04817e44abe8d5e45487bcaf4557e104afa6
jamie <hi@jamie.to>
2026-04-19 19:29:20 -0400

    feat: add getDisplayNameFallback util using user id regex courtesy of cinny

--- app/utils/matrix/user.ts
+++ app/utils/matrix/user.ts
@@ -1,6 +1,11 @@
-export type ResolveAvatarUrlOpts = Partial<{ baseUrl?: string, animated?: boolean }>
+// https://github.com/cinnyapp/cinny/blob/098684973ebb28592158efa43e79741ab27afab9/src/app/utils/matrix.ts#L26
+export const USER_ID_REG = /^([@$+#])([^\s:]+):(\S+)$/
 
-export function resolveAvatarUrl(avatarUrl: string | undefined, opts?: ResolveAvatarUrlOpts) {
+export function getDisplayNameFallback(userId: string) {
+  return userId.match(USER_ID_REG)?.at(2)
+}
+
+export function resolveAvatarUrl(avatarUrl: string | undefined, opts?: Partial<{ baseUrl?: string, animated?: boolean }>) {
   if (!avatarUrl)
     return undefined
 
