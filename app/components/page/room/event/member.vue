eb0f5aa311d02d3fd5318300554c98ebeacd8deb
jamie <hi@jamie.to>
2026-04-20 15:56:17 -0400

    feat: improve membership event rendering logic

--- app/components/page/room/event/member.vue
+++ app/components/page/room/event/member.vue
@@ -9,37 +9,81 @@ const props = defineProps<{
 assert(props.event.getType() === EventType.RoomMember, 'Event provided in PageRoomEventMember is not a RoomMember event')
 const body = computed(() => {
   const parsed = parseMembershipEvent(props.event)
-  if (parsed.type === 'ban')
-    return { icon: 'tabler:hammer', message: `**${parsed.data.bannedName}** was banned by **${parsed.data.bannerName}**` }
+  if (parsed.type === 'ban') {
+    return {
+      icon: 'tabler:hammer',
+      message: ` was banned by `,
+      sender: parsed.data.bannedName,
+      subject: parsed.data.bannerName,
+    }
+  }
 
-  if (parsed.type === 'unban')
-    return { icon: 'tabler:hammer-off', message: `**${parsed.data.unbannedName}** was unbanned by **${parsed.data.unbannerName}**` }
+  if (parsed.type === 'unban') {
+    return {
+      icon: 'tabler:hammer-off',
+      message: ` was unbanned by `,
+      sender: parsed.data.unbannedName,
+      subject: parsed.data.unbannerName,
+    }
+  }
 
   if (parsed.type === 'displayName') {
-    if (parsed.data.type === 'changed')
-      return { icon: 'tabler:label', message: `**${parsed.data.to}** changed their display name` }
+    if (parsed.data.type === 'changed') {
+      return {
+        icon: 'tabler:label',
+        message: ` changed their display name`,
+        sender: parsed.data.to,
+      }
+    }
 
-    if (parsed.data.type === 'removed')
-      return { icon: 'tabler:label-off', message: `**${parsed.data.name}** removed their display name` }
+    if (parsed.data.type === 'removed') {
+      return {
+        icon: 'tabler:label-off',
+        message: ` removed their display name`,
+        sender: parsed.data.name,
+      }
+    }
   }
 
   if (parsed.type === 'avatar') {
-    if (parsed.data.type === 'changed')
-      return { icon: 'tabler:user', message: `**${parsed.data.name}** changed their avatar` }
+    if (parsed.data.type === 'changed') {
+      return {
+        icon: 'tabler:user',
+        message: ` changed their avatar`,
+        sender: parsed.data.name,
+      }
+    }
 
-    if (parsed.data.type === 'removed')
-      return { icon: 'tabler:user-off', message: `**${parsed.data.name}** removed their avatar` }
+    if (parsed.data.type === 'removed') {
+      return {
+        icon: 'tabler:user-off',
+        message: ` removed their avatar`,
+        sender: parsed.data.name,
+      }
+    }
   }
 
-  if (parsed.type === 'leave')
-    return { icon: 'tabler:minus', message: `**${parsed.data.name}** left the room` }
+  if (parsed.type === 'leave') {
+    return {
+      icon: 'tabler:minus',
+      message: ` left the room`,
+      sender: parsed.data.name,
+    }
+  }
 
-  if (parsed.type === 'join')
-    return { icon: 'tabler:plus', message: `**${parsed.data.name}** joined the room` }
+  if (parsed.type === 'join') {
+    return {
+      icon: 'tabler:plus',
+      message: ` joined the room`,
+      sender: parsed.data.name,
+    }
+  }
 
   return {
     icon: 'tabler:question-circle',
     message: 'Unknown membership event',
+    sender: null,
+    subject: null,
     ...parsed,
   }
 })
@@ -53,6 +97,10 @@ const body = computed(() => {
     class="flex gap-2 items-center"
   >
     <Icon :name="body.icon" class="text-muted-foreground size-4" />
-    <RenderMd class="text-sm text-muted-foreground" :content="body.message" />
+    <p class="text-sm text-muted-foreground">
+      <span v-if="body.sender" class="font-medium">{{ body.sender }}</span>
+      {{ body.message }}
+      <span v-if="body.subject" class="font-medium">{{ body.subject }}</span>
+    </p>
   </PageRoomEvent>
 </template>
