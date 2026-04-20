a53f63af8038621fec5ceaef01b2a57babdf7dfe
jamie <hi@jamie.to>
2026-04-20 13:18:09 -0400

    feat: add RenderMd component

---
+++ app/components/render-md.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  content: string | undefined
+}>()
+
+const md = useMarked(() => props.content)
+</script>
+
+<template>
+  <div v-bind="$attrs" v-html="md" />
+</template>
