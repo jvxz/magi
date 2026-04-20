61b9346bfcf58ed8f082df33bca02986cd0590a7
jamie <hi@jamie.to>
2026-04-20 13:27:49 -0400

    feat: improve useEventContent

--- app/composables/use-event-content.ts
+++ app/composables/use-event-content.ts
@@ -1,18 +1,9 @@
 import type { IContent, MatrixEvent } from 'matrix-js-sdk'
 
-export function useEventContent(event: MaybeRefOrGetter<MatrixEvent | undefined>, pick?: keyof IContent, fallback?: string) {
+export function useEventContent<T extends IContent = IContent>(event: MaybeRefOrGetter<MatrixEvent | undefined>) {
   const eventRef = toRef(event)
-  const content = computed(() => {
-    if (pick)
-      return eventRef.value?.getContent()[pick] ?? fallback
+  const content = computed(() => eventRef.value?.getContent<T>())
+  const isRedacted = computed(() => eventRef.value?.isRedacted())
 
-    return eventRef.value?.getContent() ?? fallback
-  })
-
-  const isFallback = computed(() => content.value === fallback)
-
-  return {
-    content,
-    isFallback,
-  }
+  return { content, isRedacted }
 }
