702d94769c55c1a5baca32c7d345b00ab9109f1e
jamie <hi@jamie.to>
2026-04-21 14:51:09 -0400

    feat: add header animation for matrix status

--- app/components/layout/app/header.vue
+++ app/components/layout/app/header.vue
@@ -1,9 +1,14 @@
 <script lang="ts" setup>
+import { motionSlideFromTop } from '#imports'
+import { SyncState } from 'matrix-js-sdk'
+import { AnimatePresence, motion } from 'motion-v'
+
 defineProps<{
   title?: string
 }>()
 
 const currentRoom = useCurrentRoom()
+const status = useMatrixStatus()
 
 const env = computed<string | undefined>(() => {
   if (isTestMode())
@@ -19,11 +24,52 @@ const env = computed<string | undefined>(() => {
 <template>
   <header class="flex shrink-0 h-app-header-height items-center justify-between *:shrink-0">
     <div class="flex flex-1 items-center">
-      <span v-if="env" class="text-xs text-muted-foreground font-medium ml-2">{{ env }}</span>
+      <span v-if="env" class="text-xs text-muted-foreground font-medium ml-2">
+        {{ env }}
+      </span>
     </div>
-    <p class="text-sm font-medium text-center w-auto">
-      {{ title ?? currentRoom?.name ?? upperFirst($route.name) }}
-    </p>
+    <div class="text-sm font-medium text-center w-auto relative">
+      <AnimatePresence mode="popLayout" :initial="true">
+        <motion.span
+          v-if="!status.clientState"
+          key="room"
+          v-bind="motionSlideFromTop"
+        >
+          {{ title ?? currentRoom?.name ?? upperFirst($route.name) }}
+        </motion.span>
+
+        <motion.div
+          v-else
+          id="status"
+          v-bind="motionSlideFromTop"
+        >
+          <AnimatePresence mode="popLayout">
+            <motion.div
+              v-if="status.clientState === SyncState.Error"
+              key="error"
+              class="flex gap-2 items-center"
+              v-bind="motionSlideFromTop"
+            >
+              <Icon name="tabler:cloud-off" class="text-danger animate-pulse-alt" />
+              <p class="text-danger">
+                Disconnected
+              </p>
+            </motion.div>
+            <motion.div
+              v-else-if="status.clientState === SyncState.Reconnecting"
+              key="reconnecting"
+              class="flex gap-2 items-center"
+              v-bind="motionSlideFromTop"
+            >
+              <USpinner class="size-4 stroke-orange-900" />
+              <p class="text-sm text-orange-400 font-medium">
+                Reconnecting
+              </p>
+            </motion.div>
+          </AnimatePresence>
+        </motion.div>
+      </AnimatePresence>
+    </div>
     <div class="flex-1" />
   </header>
 </template>
