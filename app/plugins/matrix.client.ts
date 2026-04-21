3332e93e07c90d3203a793d63b9044585f4f31e1
jamie <hi@jamie.to>
2026-04-21 18:37:04 -0400

    feat: improve matrix plugin to prevent blocking first paint, add provided ready ref

--- app/plugins/matrix.client.ts
+++ app/plugins/matrix.client.ts
@@ -3,34 +3,63 @@ import { SyncState } from 'matrix-js-sdk'
 export default defineNuxtPlugin({
   name: 'matrix',
   order: 0,
-  setup: async () => {
-    if (isTestMode())
-      return
-
+  parallel: true,
+  setup: () => {
     const status = useMatrixStatus()
-    const { onSync } = useMatrixHooks()
+    const ready = ref(false)
 
-    // init client
-    const { client, initAuthedClient } = useMatrixClient()
+    const init = async () => {
+      try {
+        if (isTestMode())
+          return
 
-    const authPayload = await idb.get<AuthPayload>('auth')
-    if (authPayload) {
-      const authedClient = await initAuthedClient(false)
-      if (!authedClient) {
-        console.error('Unable to get authed client. Logging out...')
-        logoutClient(client.value)
+        const { onSync } = useMatrixHooks()
+
+        // init client
+        const { client, initAuthedClient } = useMatrixClient()
+
+        const authPayload = await idb.get<AuthPayload>('auth')
+        if (authPayload) {
+          const authedClient = await initAuthedClient(false)
+          if (!authedClient) {
+            console.error('Unable to get authed client. Logging out...')
+            logoutClient(client.value)
+          }
+
+          else
+            status.value.isAuthed = true
+        }
+
+        // process isDataSynced status when client changes
+        onSync((syncState, _prevState) => {
+          if (syncState === SyncState.Prepared) {
+            status.value.isDataSynced = true
+            ready.value = true
+          }
+        })
       }
-
-      else {
-        status.value.isAuthed = true
-        status.value.startupState = 'ready'
+      finally {
+        status.value.isStarting = false
       }
     }
 
-    // process isDataSynced status when client changes
-    onSync((syncState, _prevState) => {
-      if (syncState === SyncState.Prepared)
-        status.value.isDataSynced = true
+    addRouteMiddleware('auth', async (to) => {
+      if (import.meta.server || status.value.isStarting)
+        return
+
+      if (!status.value.isAuthed && to.path.includes('/app'))
+        return navigateTo('/login')
+
+      if (status.value.isAuthed && to.path.includes('/login'))
+        return navigateTo('/app')
     })
+
+    void init()
+
+    return {
+      provide: {
+        ready,
+      },
+    }
   },
 })
