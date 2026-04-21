16cb665619fa2884bcc57797bc394e263843490d
jamie <hi@jamie.to>
2026-04-21 14:48:11 -0400

    feat: enhance useMatrixStatus to include client sync state

--- app/composables/use-matrix-status.ts
+++ app/composables/use-matrix-status.ts
@@ -1,9 +1,22 @@
+import { SyncState } from 'matrix-js-sdk'
+
 export function useMatrixStatus() {
-  return useState(
+  const state = useState(
     'matrix:status',
     () => ref({
+      clientState: null as SyncState | null,
       isAuthed: isTestMode() ?? false,
       isDataSynced: isTestMode() ?? false,
     }),
   )
+
+  const { onSync } = useMatrixHooks()
+  onSync((e) => {
+    if (e === SyncState.Syncing || e === SyncState.Prepared)
+      return state.value.clientState = null
+
+    state.value.clientState = e
+  })
+
+  return state
 }
