b6c00cd48d61876111b73ab516db44f354aaa7fd
jamie <hi@jamie.to>
2026-04-21 21:05:54 -0400

    fix: reposition server guard in auth middleware

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,14 +1,14 @@
 export default defineNuxtRouteMiddleware((to) => {
   const status = useMatrixStatus()
 
+  if (import.meta.server || status.value.isStarting)
+    return
+
   if (
     isTestMode()
     && (useCookie('test-flag:skip-auth-middleware').value || localStorage.getItem('magi:test:auth'))
   )
     status.value.isAuthed = true
-
-  if (import.meta.server || status.value.isStarting)
-    return
 
   if (!status.value.isAuthed && to.path.includes('/app'))
     return navigateTo('/login')
