c8c4ab8489da9194ccadd53b5bbd1936881fde03
jamie <hi@jamie.to>
2026-04-22 16:53:39 -0400

    refactor: refactor space route middleware to its own file

---
+++ app/middleware/space.ts
@@ -0,0 +1,23 @@
+export default defineNuxtRouteMiddleware((to) => {
+  if (!('spaceId' in to.params)) {
+    return navigateTo({
+      name: 'me',
+    })
+  }
+  const lastRouteKey = getLastSpaceRouteKey(to.params.spaceId)
+  const lastRoute = localStorage.getItem(lastRouteKey)
+
+  if (to.name === 'space' && lastRoute)
+    return navigateTo(lastRoute)
+
+  if (to.name !== 'space-browse' && !('roomId' in to.params)) {
+    return navigateTo({
+      name: 'space-browse',
+      params: {
+        spaceId: to.params.spaceId,
+      },
+    })
+  }
+
+  localStorage.setItem(lastRouteKey, to.path)
+})
