fd50a868b16973587aeb7a7030cd1ddb31800395
jamie <hi@jamie.to>
2026-04-24 12:36:28 -0400

    feat(types): add MaybeUserOrId type

--- app/utils/matrix/types.ts
+++ app/utils/matrix/types.ts
@@ -1,7 +1,9 @@
-import type { ClientEventHandlerMap, EmittedEvents, EventEmitterEvents, Listener } from 'matrix-js-sdk'
+import type { ClientEventHandlerMap, EmittedEvents, EventEmitterEvents, Listener, User } from 'matrix-js-sdk'
 
 export type EmitterListener<T extends EventEmitterEvents | EmittedEvents> = Listener<EmittedEvents, ClientEventHandlerMap, T>
 
 export interface RoomMemberTypingEventContent {
   user_ids: string[]
 }
+
+export type MaybeUserOrId = User | User['userId']
