667400aafd0be4f72b793f7d918ffdf12f3e51ff
jamie <hi@jamie.to>
2026-04-24 17:31:24 -0400

    feat: add mutual rooms util + composable

---
+++ app/composables/use-mutual-rooms.ts
@@ -0,0 +1,13 @@
+export function useMutualRooms(otherUserMaybeId: MaybeRefOrGetter<MaybeUserOrId | undefined | null>) {
+  const { client } = useMatrixClient()
+  const otherUser = useUser(otherUserMaybeId)
+
+  const query = useQuery({
+    enabled: () => !!otherUser.value,
+    queryFn: () => getMutualRooms(client.value, otherUser.value?.userId),
+    queryKey: ['mutualRooms', () => otherUser.value?.userId],
+    retry: 1
+  })
+
+  return query
+}
