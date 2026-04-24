0da581c40f57d3861b15710c03ebf6186bc24735
jamie <hi@jamie.to>
2026-04-24 17:33:34 -0400

    refactor: set resize method to crop in resolveAvatarUrl util

--- app/utils/matrix/user.ts
+++ app/utils/matrix/user.ts
@@ -24,7 +24,7 @@ export function resolveAvatarUrl(avatarUrl: string | undefined, opts?: Partial<{
     animated: opts?.animated ?? true,
     baseUrl: opts?.baseUrl,
     height: size,
-    resizeMethod: 'scale',
+    resizeMethod: 'crop',
     useAuthentication: true,
     width: size,
   }, opts ||= {}))
