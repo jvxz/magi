e70fb2cd8a1a1e70a0c291cbfd21625dbfd1f563
jamie <hi@jamie.to>
2026-04-24 13:55:04 -0400

    feat: add AsyncContent component

---
+++ app/components/async-content.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts" generic="T">
+import type { PrimitiveProps } from 'reka-ui'
+import { Primitive, VisuallyHidden } from 'reka-ui'
+
+const { data, loadingLabel, ...primitiveProps } = defineProps<PrimitiveProps & {
+  data: T | null | undefined
+  loadingLabel: string
+}>()
+</script>
+
+<template>
+  <Primitive v-bind="primitiveProps">
+    <template v-if="data != null">
+      <slot :data="data!" />
+    </template>
+
+    <template v-else>
+      <VisuallyHidden>
+        <span role="status">
+          {{ loadingLabel }}
+        </span>
+      </VisuallyHidden>
+
+      <slot name="skeleton" />
+    </template>
+  </Primitive>
+</template>
