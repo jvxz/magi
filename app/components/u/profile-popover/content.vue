9aa57c182ae7580e2a3a724f215540e1f8e86689
jamie <hi@jamie.to>
2026-04-24 21:32:12 -0400

    feat: add items to homeserver dropdown + drop access to copying user id from user id

--- app/components/u/profile-popover/content.vue
+++ app/components/u/profile-popover/content.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-const { anchorElement, contentProps, user } = useProfilePopover()
+const { anchorElement, contentProps, open, user } = useProfilePopover()
 
 const profile = useUserProfile(() => user.value?.userId)
 const { self } = useSelf()
@@ -15,15 +15,7 @@ const powerLevelName = computed(() => upperFirst(isNull(powerLevel.value) ? 'mem
 
 const isSelf = computed(() => self.value?.userId === user.value?.userId)
 
-const { copy, isSupported } = useClipboard()
-const copied = refAutoReset(false, 750)
-
-function handleCopyUserId() {
-  if (!isSupported.value || !user.value?.userId)
-    return
-
-  copy(user.value.userId).then(() => copied.value = true)
-}
+const { copy } = useClipboard()
 </script>
 
 <template>
@@ -64,42 +56,30 @@ function handleCopyUserId() {
             </p>
 
             <div class="flex items-center">
-              <UTooltipRoot disable-closing-trigger @update:open="() => copied = false">
-                <UTooltipTrigger as-child :disabled="!isSupported">
-                  <button class="peer text-xs underline-offset-4 cursor-pointer hover:underline" @click="handleCopyUserId">
-                    {{ `@${parsedUserId.name}` }}
-                  </button>
-                </UTooltipTrigger>
-                <UTooltipContent
-                  side="left"
-                  class="group grid place-items-center"
-                  style="grid-template-areas: stack;"
-                  :data-copied="copied"
-                >
-                  <span
-                    style="grid-area: stack;"
-                    class="duration-100 group-data-[copied=false]:(opacity-100 scale-100) group-data-[copied=true]:(opacity-0 scale-92.5)"
-                  >
-                    Copy
-                  </span>
-                  <div
-                    style="grid-area: stack;"
-                    class="pointer-events-none duration-100 group-data-[copied=false]:(opacity-0 scale-92.5) group-data-[copied=true]:(opacity-100 scale-100)"
-                  >
-                    <Icon mode="svg" name="tabler:check" />
-                  </div>
-                </UTooltipContent>
-              </UTooltipRoot>
+              <p class="text-xs underline-offset-4">
+                {{ `@${parsedUserId.name}` }}
+              </p>
 
               <UDropdownMenuRoot>
                 <UDropdownMenuTrigger as-child>
-                  <UButton class="text-xs text-muted-foreground font-normal p-0 rounded-none border-none bg-transparent h-fit hover:text-muted-foreground peer-hover:text-foreground active:border-none hover:border-none active:bg-transparent hover:bg-transparent hover:underline peer-hover:underline">
+                  <UButton class="text-xs text-muted-foreground font-normal p-0 rounded-none border-none bg-transparent h-fit hover:text-muted-foreground active:border-none hover:border-none active:bg-transparent hover:bg-transparent hover:underline">
                     {{ `:${parsedUserId.homeserver}` }}
                   </UButton>
                 </UDropdownMenuTrigger>
                 <UDropdownMenuContent side="right" align="start">
-                  <UDropdownMenuItem>
-                    Explore rooms
+                  <UDropdownMenuItem as-child>
+                    <NuxtLink
+                      :to="{
+                        name: 'explore',
+                        params: { baseUrl: parsedUserId.homeserver },
+                      }"
+                      @click="open = false"
+                    >
+                      Explore rooms
+                    </NuxtLink>
+                  </UDropdownMenuItem>
+                  <UDropdownMenuItem @click="copy(parsedUserId.homeserver)">
+                    Copy URL
                   </UDropdownMenuItem>
                 </UDropdownMenuContent>
               </UDropdownMenuRoot>
