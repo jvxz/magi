676914d77a20691276c872798ad861c40b0f56c9
jamie <hi@jamie.to>
2026-04-27 19:16:54 -0400

    refactor: rename room list components to aside list

--- app/components/u/aside-list/tab.vue
+++ app/components/u/aside-list/tab.vue
@@ -1,20 +1,20 @@
 <script lang="ts" setup>
 import type { NuxtLinkProps } from '#app'
 import type { RouteLocationRaw } from 'vue-router'
-import { resolveRoomListTabValue } from './index.vue'
+import { resolveAsideListTabValue } from './index.vue'
 
 const props = defineProps<NuxtLinkProps>()
 
 const router = useRouter()
-const value = computed(() => resolveRoomListTabValue(props.to as RouteLocationRaw, router))
+const value = computed(() => resolveAsideListTabValue(props.to as RouteLocationRaw, router))
 </script>
 
 <template>
   <UToggleGroupItem :value as-child>
-    <URoomListButton as-child>
+    <UAsideListButton as-child>
       <NuxtLink v-bind="$props">
         <slot />
       </NuxtLink>
-    </URoomListButton>
+    </UAsideListButton>
   </UToggleGroupItem>
 </template>
