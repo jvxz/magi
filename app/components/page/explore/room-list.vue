77107fec9c7833b70119b51a3c7c958e01ba3282
jamie <hi@jamie.to>
2026-04-27 15:26:32 -0400

    feat: add dedicated room list component

---
+++ app/components/page/explore/room-list.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+defineProps<{
+  currentPage: IPublicRoomsResponse | undefined
+  hasError: boolean
+}>()
+</script>
+
+<template>
+  <div class="gap-4 grid grid-cols-4">
+    <template v-if="!hasError && currentPage">
+      <PageExploreRoom
+        v-for="room in currentPage.chunk"
+        :key="room.room_id"
+        :room="room"
+      />
+    </template>
+    <template v-else-if="!hasError && !currentPage">
+      <PageExploreRoom
+        v-for="(_, i) in Array.from({ length: 16 })"
+        :key="i"
+        :room="undefined"
+      />
+    </template>
+  </div>
+</template>
