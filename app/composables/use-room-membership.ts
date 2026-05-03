78179e18ed16d10005bce5dd89e1064618b0c1e2
jamie <hi@jamie.to>
2026-05-03 17:41:59 -0400

    feat: add useRoomMembership composable

---
+++ app/composables/use-room-membership.ts
@@ -0,0 +1,20 @@
+import type { KnownMembership } from 'matrix-js-sdk'
+
+export function useRoomMembership(maybeRoomOrId: MaybeRefOrGetter<MaybeRoomOrId | undefined>, maybeUserOrId: MaybeRefOrGetter<MaybeUserOrId | undefined>) {
+  const room = useRoom(maybeRoomOrId)
+  const user = useUser(maybeUserOrId)
+  const membership = shallowRef<KnownMembership | undefined>(getMembership())
+
+  useRoomEventHooks(maybeRoomOrId, {
+    onMembers: (_event, _state, member) => {
+      if (member.userId === user.value?.userId)
+        membership.value = member.membership as KnownMembership
+    },
+  })
+
+  function getMembership() {
+    return user.value?.userId ? room.value?.getMember(user.value?.userId)?.membership as KnownMembership : undefined
+  }
+
+  return membership
+}
