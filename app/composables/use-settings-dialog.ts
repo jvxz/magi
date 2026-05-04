80b057e606a748d173e7eadae3deb2742ce38c9a
jamie <hi@jamie.to>
2026-05-04 15:38:39 -0400

    feat: add tab ref to useSettingsDialog

--- app/composables/use-settings-dialog.ts
+++ app/composables/use-settings-dialog.ts
@@ -1,7 +1,9 @@
 export const useSettingsDialog = createGlobalState(() => {
   const open = shallowRef(false)
+  const tab = shallowRef<SettingsCategory>(SETTINGS_DEFAULT_TAB)
 
   return {
     open,
+    tab,
   }
 })
