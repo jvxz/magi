eb98e8dd0c099032359d612979fa8bef6f4fdb45
jamie <hi@jamie.to>
2026-05-04 16:54:57 -0400

    fix: fix matrix event type schema

--- app/constants/matrix/schemas.ts
+++ app/constants/matrix/schemas.ts
@@ -1,4 +1,4 @@
 import { EventType } from 'matrix-js-sdk'
-import * as v from "valibot"
+import * as v from 'valibot'
 
-export const EventTypeSchema = v.union(objectValues(EventType).map(v.literal))
+export const EventTypeSchema = v.picklist(objectValues(EventType))
