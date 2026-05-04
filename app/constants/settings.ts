9cb7aa5cc979b433f9415b36ddeb7327de7eadaa
jamie <hi@jamie.to>
2026-05-04 16:55:10 -0400

    feat: enhance settings schema types + add defaults

--- app/constants/settings.ts
+++ app/constants/settings.ts
@@ -1,10 +1,14 @@
+import { EventType } from 'matrix-js-sdk'
 import * as v from 'valibot'
 
 export const SETTINGS_DEFAULT_TAB: SettingsCategory = 'accessibility'
 
 export const AppearanceSettingsSchema = v.pipe(
   v.object({
-    font: v.union([v.literal('Inter'), v.literal('System')]),
+    font: v.optional(
+      v.union([v.literal('Inter'), v.literal('System')]),
+      'System',
+    ),
   }),
   v.metadata({
     icon: 'tabler:palette',
@@ -14,8 +18,8 @@ export const AppearanceSettingsSchema = v.pipe(
 
 export const AccessibilitySettingsSchema = v.pipe(
   v.object({
-    doAnimations: v.boolean(),
-    saturationPercent: v.number(),
+    doAnimations: v.optional(v.boolean(), true as boolean),
+    saturationPercent: v.optional(v.number(), 50 as number),
   }),
   v.metadata({
     icon: 'tabler:accessible',
@@ -25,7 +29,10 @@ export const AccessibilitySettingsSchema = v.pipe(
 
 export const EventSettingsSchema = v.pipe(
   v.object({
-    toggledEventTypes: v.array(EventTypeSchema),
+    toggledEventTypes: v.optional(v.array(EventTypeSchema), [
+      EventType.RoomMessage,
+      EventType.RoomMember,
+    ]),
   }),
   v.metadata({
     icon: 'tabler:calendar-event',
@@ -33,20 +40,18 @@ export const EventSettingsSchema = v.pipe(
   }),
 )
 
-export const SettingsSchema = v.pipe(
-  v.object({
-    accessibility: AccessibilitySettingsSchema,
-    appearance: AppearanceSettingsSchema,
-    events: EventSettingsSchema,
-  }),
-  v.metadata({
-    icon: 'tabler:settings',
-    title: 'Settings',
-  }),
-)
+const settingsSchemas = {
+  accessibility: AccessibilitySettingsSchema,
+  appearance: AppearanceSettingsSchema,
+  events: EventSettingsSchema,
+} as const
 
-export type Settings = v.InferOutput<typeof SettingsSchema>
-export type SettingsCategory = keyof Settings
+export const SettingsSchema = v.object(settingsSchemas)
+
+export type Settings = {
+  [K in SettingsCategory]: v.InferOutput<(typeof settingsSchemas)[K]>
+}
+export type SettingsCategory = keyof typeof settingsSchemas
 
 export const SETTINGS_CATEGORIES = objectKeys(SettingsSchema.entries)
 export const SETTINGS_ENTRIES = mapValues(SettingsSchema.entries, v.getMetadata)
