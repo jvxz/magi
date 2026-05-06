0dcf0c9309039c8ef73d9417bc2f7ce109772663
jamie <hi@jamie.to>
2026-05-05 20:27:06 -0400

    feat: add settings form components

---
+++ app/components/settings/form/autocomplete.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup generic="T extends string[] | undefined">
+import type { SettingsFormPrimitiveProps } from './types'
+
+type ValueType = T extends string[] ? T[number] : string
+
+defineProps<SettingsFormPrimitiveProps & {
+  options: T
+  defaultOption?: ValueType
+}>()
+
+const modelValue = defineModel<ValueType>()
+</script>
+
+<template>
+  <SettingsFormPrimitive v-bind="$props">
+    <USelectRoot v-model:model-value="modelValue">
+      <USelectTrigger class="min-w-48">
+        <USelectValue>
+          {{ modelValue }}
+        </USelectValue>
+      </USelectTrigger>
+      <USelectContent>
+        <USelectItem
+          v-for="option in options"
+          :key="option"
+          :value="option"
+        >
+          <USelectItemText>
+            {{ option }}
+          </USelectItemText>
+        </USelectItem>
+      </USelectContent>
+    </USelectRoot>
+  </SettingsFormPrimitive>
+</template>
---
+++ app/components/settings/form/primitive.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import type { FormPrimitiveProps } from '~/components/form/primitive.vue'
+
+defineProps<FormPrimitiveProps & { description?: string }>()
+</script>
+
+<template>
+  <FormPrimitive v-bind="$props" class="flex gap-2 items-center justify-between space-y-0">
+    <template #label="{ id }">
+      <div class="flex flex-col gap-1">
+        <ULabel :for="id" class="text-base font-medium m-0">
+          {{ label }}
+        </ULabel>
+        <p v-if="description" class="text-sm text-muted-foreground">
+          {{ description }}
+        </p>
+      </div>
+    </template>
+    <slot />
+  </FormPrimitive>
+</template>
