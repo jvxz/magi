d1b995cc03fad4081059c148dca7e32d55dd1f8f
jamie <hi@jamie.to>
2026-05-05 20:26:10 -0400

    refactor: edit settings content layout component

--- app/components/settings/content-layout.vue
+++ app/components/settings/content-layout.vue
@@ -5,7 +5,7 @@ defineProps<PrimitiveProps>()
 </script>
 
 <template>
-  <Primitive v-bind="$props" :class="cn('flex flex-col p-3.5', $attrs.class)">
+  <Primitive v-bind="$props" :class="cn('mt-12 flex flex-col p-3.5 mx-auto max-w-screen-md', $attrs.class)">
     <slot />
   </Primitive>
 </template>
