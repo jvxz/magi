8b7d04a3856d3b944d9f5e2d75788cd04ef2e9b1
jamie <hi@jamie.to>
2026-05-05 20:27:13 -0400

    feat: add settings content components

---
+++ app/components/settings/content/appearance.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const settings = useSettings()
+</script>
+
+<template>
+  <SettingsContentLayout>
+    <SettingsFormAutocomplete
+      v-model:model-value="settings.appearance.font"
+      :label="SETTINGS_ITEM_METADATA.appearance.font.title"
+      :description="SETTINGS_ITEM_METADATA.appearance.font.description"
+      :options="SETTINGS_ITEM_METADATA.appearance.font.options"
+      :default-option="DEFAULT_SETTINGS.appearance.font"
+    />
+  </SettingsContentLayout>
+</template>
