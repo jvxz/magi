33b23237ce569919af8b5d7d84a187d02dfbfc2f
jamie <hi@jamie.to>
2026-05-08 13:11:32 -0400

    feat: add generic page app layout component

---
+++ app/components/layout/app/generic-page.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import type { PrimitiveProps } from 'reka-ui'
+
+defineProps<PrimitiveProps & {
+  classes?: DefineClasses<'container' | 'shell'>
+}>()
+</script>
+
+<template>
+  <Primitive v-bind="$props" :class="cn('py-page-y-padding h-full relative overflow-y-scroll scrollbar-gutter-stable', $props.classes?.shell)">
+    <div :class="cn('mx-auto container max-w-screen-xl space-y-lg', $props.classes?.container)">
+      <slot />
+    </div>
+  </Primitive>
+</template>
