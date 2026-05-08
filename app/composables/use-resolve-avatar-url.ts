4943e10a08cec0a29d79b32d3651c568223453ed
jamie <hi@jamie.to>
2026-05-08 13:21:35 -0400

    feat: enhance useResolveAvatarUrl

--- app/composables/use-resolve-avatar-url.ts
+++ app/composables/use-resolve-avatar-url.ts
@@ -1,1 +1,16 @@
-export const useResolveAvatarUrl = reactify(resolveAvatarUrl)
+export function useResolveAvatarUrl(url: MaybeRefOrGetter<string | undefined>, opts?: MaybeRefOrGetter<ResolveAvatarUrlOpts>) {
+  const urlRef = toRef(url)
+  const optsRef = toRef(opts)
+  const { client } = useMatrixClient()
+  const reducedMotion = usePreferredReducedMotion()
+
+  const resolvedUrl = computed(() => resolveAvatarUrl(urlRef.value, merge<ResolveAvatarUrlOpts, ResolveAvatarUrlOpts>({
+    allowDirectLinks: true,
+    allowRedirects: true,
+    animated: reducedMotion.value !== 'reduce',
+    baseUrl: client.value.getHomeserverUrl(),
+    useAuthentication: true,
+  }, optsRef.value ?? {})))
+
+  return resolvedUrl
+}
