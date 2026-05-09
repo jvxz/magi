010052eb57cf5cab87e168cec4cdf04d6a8b3ed7
jamie <hi@jamie.to>
2026-05-09 15:58:21 -0400

    feat: improve avatar component

--- app/components/matrix/avatar.vue
+++ app/components/matrix/avatar.vue
@@ -1,13 +1,14 @@
 <script lang="ts">
-import type { Room } from 'matrix-js-sdk'
 import type { ImgProps } from '~/components/img.vue'
 
 export type MatrixAvatarProps = Omit<ImgProps, 'src' | 'alt'> & {
   square?: boolean
-  room?: Room | undefined
-  user: MaybeUserOrId | undefined | null
   imageSize?: AvatarImageSize
-}
+} & (
+  | { room: MaybeRoomOrId | undefined | null, user?: never, src?: never }
+  | { user: MaybeUserOrId | undefined | null, room?: never, src?: never }
+  | { src: string | undefined | null, room?: never, user?: never }
+)
 </script>
 
 <script setup lang="ts">
@@ -18,16 +19,32 @@ const props = withDefaults(
   },
 )
 
-const userProfile = useUserProfile(() => typeof props.user === 'string' ? props.user : props.user?.userId)
-const resolvedAvatar = useResolveAvatarUrl(() => userProfile.value?.avatar_url, { size: props.imageSize })
+const room = useRoom(() => props.room ?? undefined)
+const userProfile = useUserProfile(() => props.user ?? undefined)
+
+const src = computed(() => {
+if (props.src) return props.src
+
+  if (room.value)
+    return room.value.getMxcAvatarUrl() ?? undefined
+
+  if (userProfile.value)
+    return userProfile.value.avatar_url
+
+    return undefined
+})
+
+const resolvedAvatar = useResolveAvatarUrl(src, { size: props.imageSize })
 
 const isError = ref(false)
+
+const delegatedProps = reactiveOmit(props, 'room', 'user', 'src', 'square', 'imageSize')
 </script>
 
 <template>
   <Img
     v-if="resolvedAvatar"
-    v-bind="props"
+    v-bind="delegatedProps"
     :key="resolvedAvatar"
     data-slot="avatar"
     :alt="userProfile?.displayname ? `${userProfile?.displayname}'s avatar` : 'Avatar'"
