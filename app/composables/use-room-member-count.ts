28a62a225471ad5a7835831a8905203833429427
jamie <hi@jamie.to>
2026-05-11 15:11:13 -0400

    feat: add useRoomMemberCount composables

---
+++ app/composables/use-room-member-count.ts
@@ -0,0 +1,25 @@
+import { KnownMembership } from 'matrix-js-sdk'
+
+export function useRoomMemberCount(roomOrId: MaybeRefOrGetter<MaybeRoomOrId | undefined>) {
+  const room = useRoom(roomOrId)
+  const count = shallowRef(room.value?.getJoinedMemberCount() ?? undefined)
+  const { onRoomMembership } = useMatrixHooks()
+
+  const update = () => count.value = room.value?.getJoinedMemberCount() ?? undefined
+
+  onRoomMembership(async (event, member, oldMembership) => {
+    if (member.roomId === room.value?.roomId) {
+      const { type } = parseMembershipEvent(event)
+      if (type === 'join')
+        count.value = (count.value ?? 0) + 1
+      else if (type === 'leave' || type === 'kick')
+        count.value = (count.value ?? 0) - 1
+      else if (type === 'ban' && oldMembership === KnownMembership.Join)
+        count.value = (count.value ?? 0) - 1
+      else update()
+    }
+  })
+  watch(room, update)
+
+  return count
+}
