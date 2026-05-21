6d964a6dec1a4f3f4381c9d655dbd70357333e74
jamie <hi@jamie.to>
2026-05-21 13:35:57 -0400

    feat: enhance room event component with context menu

--- app/components/page/room/event.vue
+++ app/components/page/room/event.vue
@@ -1,33 +1,51 @@
 <script lang="ts" setup>
-import type { EventType } from 'matrix-js-sdk'
+import type { CompactEmoji } from 'emojibase'
+import type { EventType, MatrixEvent } from 'matrix-js-sdk'
 import type { PrimitiveProps } from 'reka-ui'
 import type { HTMLAttributes } from 'vue'
+import { ContextMenuSubContent } from '#components'
 
-withDefaults(
+const props = withDefaults(
   defineProps<
     PrimitiveProps & {
       class?: HTMLAttributes['class']
-      eventId: string | undefined
+      event?: MatrixEvent | undefined
       eventType: EventType | string
       grouped?: boolean
+      room: MaybeRoomOrId | undefined
     }
   >(),
   {
     as: 'div',
   },
 )
+
+const mounted = ref(true)
+const { reactTo } = useRoomEventReactions.provide(
+  () => props.room,
+  () => props.event,
+)
+
+function onEmojiPick(emoji: CompactEmoji) {
+  if (props.event) {
+    reactTo(emoji.unicode, true)
+  }
+
+  mounted.value = false
+  nextTick(() => (mounted.value = true))
+}
 </script>
 
 <template>
-  <UContextMenu>
+  <UContextMenu v-if="mounted" modal>
     <UContextMenuTrigger
       v-bind="$props"
       data-testid="event-root"
-      :data-event-id="eventId"
+      :data-event-id="event?.getId()"
       :data-event-type="eventType"
       :class="
         cn(
-          'px-6 data-[grouped=false]:mt-4.5 group hover:bg-card-lighter data-[popover-open]:bg-card-lighter shrink-0 group data-[grouped=true]:min-h-0',
+          'px-6 data-[grouped=false]:mt-4.5 group hover:bg-card-lighter data-[popover-open]:bg-card-lighter shrink-0 data-[grouped=true]:min-h-0',
           $props.class,
         )
       "
@@ -36,12 +54,23 @@ withDefaults(
       <slot />
     </UContextMenuTrigger>
 
-    <UContextMenuContent>
-      <UContextMenuItem> Edit </UContextMenuItem>
-      <UContextMenuItem> Reply </UContextMenuItem>
-      <UContextMenuItem> Quote </UContextMenuItem>
-      <UContextMenuItem> Copy </UContextMenuItem>
-      <UContextMenuItem> Delete </UContextMenuItem>
+    <UContextMenuContent v-if="event">
+      <div class="flex items-center justify-around">
+        <ContextMenuItem as-child>
+          <UButton v-for="i in 4" :key="i" size="icon" variant="ghost" class="grow h-full aspect-square cursor-default">
+            🥺
+          </UButton>
+        </ContextMenuItem>
+      </div>
+      <UContextMenuSub>
+        <UContextMenuSubTrigger> Add reaction </UContextMenuSubTrigger>
+
+        <UEmojiPickerRoot :as="ContextMenuSubContent" @pick="onEmojiPick">
+          <UEmojiPickerSearch />
+          <ContextMenuItem as-child><UEmojiPickerList /></ContextMenuItem>
+        </UEmojiPickerRoot>
+      </UContextMenuSub>
+      <UContextMenuItem> View reactions </UContextMenuItem>
     </UContextMenuContent>
   </UContextMenu>
 </template>
