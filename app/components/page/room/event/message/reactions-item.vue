dcbc3fac6ebd20109a91e12ae4edcd52194f6b90
jamie <hi@jamie.to>
2026-05-21 14:01:37 -0400

    feat: add mockup tooltip for reaction items

--- app/components/page/room/event/message/reactions-item.vue
+++ app/components/page/room/event/message/reactions-item.vue
@@ -24,25 +24,36 @@ watch(error, () => (localCount.value = realCount.value))
 
 <template>
   <div v-if="localCount" class="py-1">
-    <UToggle
-      :key="reaction"
-      v-model:model-value="localIsReacting"
-      size="sm"
-      class="text-sm p-1 px-2 border-px rounded flex gap-2 min-w-12 select-none items-center overflow-clip data-[state=off]:(border-transparent bg-card-lightest) data-[state=on]:(border-primary bg-primary/50 hover:bg-primary/50)"
-      :data-count="realCount"
-      @mouseover="getReactors(reaction)"
-    >
-      <span class="truncate">{{ reaction }}</span>
+    <UTooltipProvider :delay-duration="700">
+      <UTooltipRoot>
+        <UTooltipTrigger as-child>
+          <UToggle
+            :key="reaction"
+            v-model:model-value="localIsReacting"
+            size="sm"
+            class="text-sm text-foreground p-1 px-2 border-px rounded flex gap-2 min-w-12 select-none items-center overflow-clip aria-[pressed=false]:(border-transparent bg-card-lightest) aria-[pressed=true]:(border-primary bg-primary/50 hover:bg-primary/50)"
+            :data-count="realCount"
+            @mouseover="getReactors(reaction)"
+          >
+            <span class="truncate">{{ reaction }}</span>
 
-      <div class="h-1lh relative">
-        <span class="font-semibold opacity-0 select-none tabular-nums" aria-hidden="true">{{ localCount }}</span>
+            <div class="h-1lh relative">
+              <span class="font-semibold opacity-0 select-none tabular-nums" aria-hidden="true">{{ localCount }}</span>
 
-        <Transition :name="prevLocalCount > localCount ? 'flip-up' : 'flip-down'">
-          <span :key="localCount" class="font-semibold flex items-center inset-0 justify-center absolute tabular-nums">
-            {{ localCount }}
-          </span>
-        </Transition>
-      </div>
-    </UToggle>
+              <Transition :name="prevLocalCount > localCount ? 'flip-up' : 'flip-down'">
+                <span
+                  :key="localCount"
+                  class="font-semibold flex items-center inset-0 justify-center absolute tabular-nums"
+                >
+                  {{ localCount }}
+                </span>
+              </Transition>
+            </div>
+          </UToggle>
+        </UTooltipTrigger>
+
+        <UTooltipContent>test</UTooltipContent>
+      </UTooltipRoot>
+    </UTooltipProvider>
   </div>
 </template>
