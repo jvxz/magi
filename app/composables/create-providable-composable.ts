042bc3c03fef69bc0757ad0aa7053abdc12efe34
jamie <hi@jamie.to>
2026-05-21 12:21:27 -0400

    chore: add documentation to createProvidableComposable

--- app/composables/create-providable-composable.ts
+++ app/composables/create-providable-composable.ts
@@ -8,6 +8,22 @@ interface ProvidableComposable<Args extends any[], R> {
   inject: () => R
 }
 
+/**
+ * composable that optionally be provided from the parent & injected into children
+ * @param name parent component name
+ * @param composable composable factory
+ * @example
+ * ```ts
+ * // regular composable
+ * const count = useCount(5)
+ *
+ * // provided composable (parent component)
+ * const count = useCount.provide(5)
+ *
+ * // injected composable (child, same instance as parent)
+ * const count = useCount.inject() // no arguments, instantiated from parent
+ * ```
+ */
 export function createProvidableComposable<Args extends any[], R>(
   name: string,
   composable: Composable<Args, R>,
