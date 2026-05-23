e7760b1b50887768c48d567b4c0d0daa0e13818c
jamie <hi@jamie.to>
2026-05-23 14:45:31 -0400

    refactor: add stable z index shortcuts to unocss

--- app/components/u/dialog/dialog-content.vue
+++ app/components/u/dialog/dialog-content.vue
@@ -20,7 +20,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
       :class="
         cn(
           staticBase({ variant: 'default' }),
-          'fixed top-[50%] left-[50%] overflow-clip z-50 size-full rounded-none border-0 md:border md:rounded md:h-[90%] md:w-352 translate-x-[-50%] translate-y-[-50%] gap-4 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0 data-[state=closed]:animate-out data-[state=open]:zoom-in-95 p-0',
+          'fixed top-[50%] left-[50%] overflow-clip z-dialog size-full rounded-none border-0 md:border md:rounded md:h-[90%] md:w-352 translate-x-[-50%] translate-y-[-50%] gap-4 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0 data-[state=closed]:animate-out data-[state=open]:zoom-in-95 p-0',
           props.class,
         )
       "
