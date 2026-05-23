6206d8ea78128a4f021b87c4ab4cc875bc248123
jamie <hi@jamie.to>
2026-05-23 13:17:15 -0400

    feat: enhance dialog components

--- app/components/u/dialog/dialog-header.vue
+++ app/components/u/dialog/dialog-header.vue
@@ -1,13 +1,31 @@
-<script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
+<script lang="ts" setup>
+import type { PrimitiveProps } from 'reka-ui'
 
-const props = defineProps<{
-  class?: HTMLAttributes['class']
-}>()
+withDefaults(defineProps<PrimitiveProps & { withClose?: boolean }>(), { as: 'header', withClose: true })
 </script>
 
 <template>
-  <div :class="cn('flex flex-col gap-2 text-center sm:text-left', props.class)">
+  <Primitive
+    v-bind="$props"
+    :class="
+      cn(
+        'gap-2 text-center sm:text-left pe-2.5 ps-4 border-b flex shrink-0 h-header-height items-center justify-between',
+        $attrs.class,
+      )
+    "
+  >
     <slot />
-  </div>
+    <DialogClose
+      v-if="withClose"
+      :class="
+        cn(
+          interactiveBase({ variant: 'ghost', size: 'icon' }),
+          'inline-flex size-8 items-center justify-center opacity-70',
+        )
+      "
+    >
+      <Icon name="tabler:x" class="size-5" />
+      <VisuallyHidden>Close</VisuallyHidden>
+    </DialogClose>
+  </Primitive>
 </template>
