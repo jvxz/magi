2d5d9c1895cbe128e4e21e202aba61af2b284e8a
jamie <hi@jamie.to>
2026-05-23 13:17:47 -0400

    feat: add UseRoomMember renderless component

---
+++ app/components/use/room-member.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  room: MaybeRoomOrId | undefined
+  user: MaybeUserOrId | undefined
+}>()
+
+const roomMember = useRoomMember(
+  () => props.room,
+  () => props.user,
+)
+</script>
+
+<template>
+  <slot :room-member />
+</template>
