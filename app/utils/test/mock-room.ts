1bd55a1c5828edb875748fc7ce2b788891d37768
jamie <hi@jamie.to>
2026-05-25 16:13:10 -0400

    fix: fix type error

--- app/utils/test/mock-room.ts
+++ app/utils/test/mock-room.ts
@@ -1,4 +1,3 @@
-import type { User } from 'matrix-js-sdk'
 import { MatrixEvent, Room, RoomMember } from 'matrix-js-sdk'
 
 const DEFAULT_SENDER = '@test:localhost'
@@ -9,7 +8,6 @@ export interface MockRoom {
   pushMessage: (opts?: PushMessageOptions) => MatrixEvent
   pushReaction: (target: MatrixEvent, key: string, opts?: PushReactionOptions) => MatrixEvent
   redact: (event: MatrixEvent) => void
-  createUser: (userId: string) => User
 }
 
 interface PushMessageOptions {
