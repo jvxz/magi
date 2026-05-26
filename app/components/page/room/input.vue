52949db5ed09eb00b0fe75ee78798efcd0fd5c52
jamie <hi@jamie.to>
2026-05-26 15:28:49 -0400

    refactor: improve aria attributes in typing indicator animation

--- app/components/page/room/input.vue
+++ app/components/page/room/input.vue
@@ -25,8 +25,7 @@ const { areMembersTyping } = useRoomMembersTyping.provide(currentRoom)
     </div>
 
     <div
-      aria-hidden
-      aria-busy
+      aria-hidden="true"
       class="h-full w-full pointer-events-none transition-all ease absolute from-card to-transparent from-75% bg-linear-to-t -mx-3 -z-2"
       :class="areMembersTyping ? '-top-2' : 'top-2'"
     />
