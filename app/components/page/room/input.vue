4af34308d17ebdb119ae8a91137a4001f3acdb24
jamie <hi@jamie.to>
2026-05-26 14:42:56 -0400

    feat: add members typing component above room input component

--- app/components/page/room/input.vue
+++ app/components/page/room/input.vue
@@ -4,11 +4,18 @@ const currentRoom = useCurrentRoom()
 const placeholder = computed(() => (currentRoom.value ? `Message in ${currentRoom.value.name}` : '...'))
 
 const msg = shallowRef('')
+
+const { typingMembers } = useRoomMembersTyping.provide(currentRoom)
+const areMembersTyping = computed(() => typingMembers.value.size > 0)
 </script>
 
 <template>
-  <div class="mb-3 px-3 rounded flex shrink-0 h-user-card-height w-full bottom-0 absolute overflow-hidden">
-    <div class="px-3.5 border rounded bg-card-lighter flex gap-3.5 size-full items-center *:shrink-0">
+  <div class="mb-3 px-3 rounded flex-col flex shrink-0 w-full bottom-0 absolute isolate">
+    <PageRoomInputMembersTyping />
+
+    <div
+      class="px-3.5 border h-user-card-height rounded bg-card-lighter flex gap-3.5 size-full items-center *:shrink-0"
+    >
       <UButton variant="ghost" size="icon">
         <Icon name="tabler:plus" class="size-5" />
       </UButton>
@@ -17,5 +24,12 @@ const msg = shallowRef('')
         <Icon name="tabler:send" class="size-5" />
       </UButton>
     </div>
+
+    <div
+      aria-hidden
+      aria-busy
+      class="absolute w-full h-full -mx-3 pointer-events-none -z-2 bg-linear-to-t ease transition-all from-card to-transparent from-75%"
+      :class="areMembersTyping ? '-top-2' : 'top-2'"
+    />
   </div>
 </template>
