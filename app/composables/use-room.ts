3e6fe222befaf05299471acc5c546d337545ecf2
jamie <hi@jamie.to>
2026-05-27 16:18:14 -0400

    fix: use `watchEffect` in `useRoom`

--- app/composables/use-room.ts
+++ app/composables/use-room.ts
@@ -6,14 +6,11 @@ export function useRoom(roomInput: MaybeRefOrGetter<MaybeRoomOrId | undefined>) 
   const roomEffectScope = useRoomEffectScope(roomInput)
   const roomRef = shallowRef<Room>()
 
-  watch(
-    inputRef,
-    input => {
-      roomRef.value = input instanceof Room ? markRaw(input) : roomEffectScope.value?.ref.value
-      triggerRef(roomRef)
-    },
-    { immediate: true },
-  )
+  watchEffect(() => {
+    const input = inputRef.value
+    roomRef.value = input instanceof Room ? markRaw(input) : roomEffectScope.value?.ref.value
+    triggerRef(roomRef)
+  })
 
   return roomRef
 }
