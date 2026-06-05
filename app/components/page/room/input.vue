2c3cb1f6263b72fa7fdcfc09969e2a0e4a3a9559
Jamie <hi@jamie.to>
2026-06-05 13:39:58 -0400

    feat: enhance room page input component

--- app/components/page/room/input.vue
+++ app/components/page/room/input.vue
@@ -1,31 +1,289 @@
 <script lang="ts" setup>
+import type { MentionNodeAttrs } from '@tiptap/extension-mention'
+import type { SuggestionOptions } from '@tiptap/suggestion'
+import type { RoomMember, User } from 'matrix-js-sdk'
+import type { IHierarchyRoom } from 'matrix-js-sdk/lib/@types/spaces'
+
+import { Extension } from '@tiptap/core'
+import { Emoji } from '@tiptap/extension-emoji'
+import { Mention } from '@tiptap/extension-mention'
+import { Placeholder } from '@tiptap/extension-placeholder'
+import { useFilter } from 'reka-ui'
+import CodeBlockShiki from 'tiptap-extension-code-block-shiki'
+import { VList } from 'virtua/vue'
+
 const currentRoom = useCurrentRoom()
+const currentSpace = useCurrentSpace()
 
-const placeholder = computed(() => (currentRoom.value ? `Message in ${currentRoom.value.name}` : '...'))
-
-const msg = shallowRef('')
+const content = shallowRef('')
 
 const { areMembersTyping } = useRoomMembersTyping.provide(currentRoom)
+
+const { contains } = useFilter({ sensitivity: 'base' })
+
+const { members } = useRoomMembers(currentRoom)
+const { rooms } = useSpaceHierarchy(() => currentSpace.value?.roomId)
+const roomsArray = computed(() => (rooms.value ? rooms.value.values().toArray() : []))
+
+type SuggestionItem = { id: string; label: string } & (
+  | {
+      user: RoomMember | User
+      room?: never
+    }
+  | {
+      user?: never
+      room: IHierarchyRoom
+    }
+)
+
+const open = shallowRef(false)
+const highlightedIdx = shallowRef(0)
+const filteredItems = shallowRef<SuggestionItem[]>([])
+let command: ((props: MentionNodeAttrs) => void) | undefined
+
+const listHeight = computed(() => Math.min(filteredItems.value.length, 7) * 32)
+
+function selectItem(item: SuggestionItem) {
+  command?.({ id: item.id, label: item.label })
+}
+
+function createSuggestion(
+  char: string,
+  getItems: (query: string) => SuggestionItem[],
+): Omit<SuggestionOptions<any, MentionNodeAttrs>, 'editor'> {
+  return {
+    char,
+    items: ({ query }) => getItems(query),
+    render: () => ({
+      onExit: () => (open.value = false),
+      onKeyDown: ({ event }) => handleKeyDown(event),
+      onStart: ({ command: commandLocal, items }) => {
+        command = commandLocal
+        filteredItems.value = items
+        highlightedIdx.value = 0
+        open.value = true
+      },
+      onUpdate: ({ command: commandLocal, items }) => {
+        command = commandLocal
+        filteredItems.value = items
+        highlightedIdx.value = 0
+      },
+    }),
+  }
+}
+
+const editor = useEditor({
+  extensions: [
+    TiptapStarterKit.configure({
+      bold: false,
+      code: false,
+      codeBlock: false,
+      heading: false,
+      italic: false,
+      link: false,
+      strike: false,
+      trailingNode: false,
+      underline: false,
+    }),
+    CodeBlockShiki.configure({
+      defaultTheme: 'github-dark-default',
+    }),
+    Emoji,
+    Mention.configure({
+      suggestions: [
+        createSuggestion('#', query =>
+          roomsArray.value
+            .filter(r => r.name && contains(r.name, query))
+            .map(r => ({ id: r.room_id, label: r.name ?? r.room_id, room: r })),
+        ),
+        createSuggestion('@', query =>
+          (members.value ?? [])
+            .filter(u => contains([resolveUserName(u), u.userId].join(' '), query))
+            .map(u => ({ id: u.userId, label: resolveUserName(u), user: u })),
+        ),
+      ],
+    }),
+    InlineMarks,
+    Placeholder.configure({
+      placeholder: `Message ${currentRoom.value?.name}`,
+    }),
+    Extension.create({
+      addKeyboardShortcuts() {
+        return {
+          Escape: () => {
+            this.editor.commands.blur()
+            return true
+          },
+        }
+      },
+    }),
+  ],
+})
+
+function handleKeyDown(event: KeyboardEvent) {
+  const items = filteredItems.value
+  if (!items.length) return false
+  switch (event.key) {
+    case 'ArrowUp':
+      highlightedIdx.value = clamp(highlightedIdx.value - 1, 0, items.length - 1)
+      return true
+    case 'ArrowDown':
+      highlightedIdx.value = clamp(highlightedIdx.value + 1, 0, items.length - 1)
+      return true
+    case 'Enter':
+      selectItem(items[highlightedIdx.value]!)
+      return true
+    case 'Escape':
+      open.value = false
+      return true
+  }
+  return false
+}
+
+onStartTyping(e => editor.value?.chain().focus('end').insertContent(e.key).run())
+
+const vlist = useTemplateRef('vlist')
+watch(highlightedIdx, idx => vlist.value?.scrollToIndex(idx, { align: 'nearest' }))
 </script>
 
 <template>
   <div class="mb-3 px-3 rounded flex shrink-0 flex-col w-full bottom-0 absolute isolate">
-    <PageRoomInputMembersTyping />
+    <PageRoomInputMembersTyping v-if="!open" />
 
-    <div class="px-3.5 border rounded bg-input flex gap-3.5 size-full h-user-card-height items-center *:shrink-0">
+    <div v-if="open" :class="cn(popoverContentBase(), 'w-full border-border-strong h-full bg-secondary mb-3.5')">
+      <VList
+        v-if="filteredItems.length"
+        :data="filteredItems"
+        ref="vlist"
+        v-slot="{ item, index }"
+        :style="{
+          minHeight: `${listHeight}px`,
+        }"
+      >
+        <UButton
+          :key="item.id"
+          variant="ghost"
+          :data-highlighted="highlightedIdx === index"
+          @mouseenter="highlightedIdx = index"
+          @mousedown.prevent="selectItem(item)"
+          class="w-full justify-start gap-2 data-[highlighted=true]:(bg-secondary-raised hover:bg-secondary-raised text-foreground) data-[highlighted=false]:(bg-transparent text-muted-foreground hover:text-muted-foreground hover:bg-transparent)"
+        >
+          <MatrixAvatar v-if="item.user" class="h-4 w-fit" :user="item.user" />
+          <MatrixAvatar v-else class="h-4 w-fit" :room="item.room" />
+
+          <span class="text-foreground">{{ item.label }}</span>
+          <span class="text-xs font-normal">{{ item.id }}</span>
+        </UButton>
+      </VList>
+
+      <UButton v-else variant="ghost" disabled class="w-full justify-start gap-2 opacity-100!">
+        <span class="text-muted-foreground font-normal">No results</span>
+      </UButton>
+    </div>
+
+    <div
+      class="px-3.5 border has-focus-visible:border-border-strong rounded bg-input flex gap-3.5 size-full h-user-card-height items-center *:shrink-0"
+    >
       <UButton variant="ghost" size="icon">
         <Icon name="tabler:plus" class="size-5" />
       </UButton>
-      <input v-model="msg" type="text" class="text-sm font-sans outline-none flex-1 shrink size-full" :placeholder />
-      <UButton variant="ghost" size="icon" :disabled="!msg">
+
+      <TiptapEditorContent class="flex-1" role="presentation" :editor />
+
+      <UButton variant="ghost" size="icon" :disabled="!content">
         <Icon name="tabler:send" class="size-5" />
       </UButton>
     </div>
 
     <div
+      v-if="!open"
       aria-hidden="true"
       class="h-full w-full pointer-events-none transition-all ease absolute from-surface to-transparent from-75% bg-linear-to-t -mx-3 -z-2"
       :class="areMembersTyping ? '-top-2' : 'top-2'"
     />
   </div>
 </template>
+
+<style>
+.ProseMirror {
+  @apply outline-0 h-user-card-height flex flex-col justify-center;
+}
+
+.tiptap {
+  .md-marker {
+    @apply text-muted-foreground;
+    text-decoration: none;
+    font-style: normal;
+  }
+
+  .md-codespan:not(.md-marker),
+  pre {
+    @apply bg-muted font-mono px-0.5 -m-0.25 mx-0.75 rounded-sm;
+  }
+
+  .md-em:not(.md-marker) {
+    @apply italic;
+  }
+
+  .md-link:not(.md-marker) {
+    @apply underline text-primary cursor-pointer;
+  }
+
+  .md-strikethrough:not(.md-marker) {
+    text-decoration: line-through;
+  }
+
+  .md-strong:not(.md-marker) {
+    @apply font-medium;
+  }
+
+  .md-underline:not(.md-marker) {
+    @apply underline;
+  }
+
+  [data-type='mention'] {
+    @apply bg-primary/50 px-0.5 -m-0.25 mx-0.75 rounded-sm;
+    user-select: auto;
+  }
+
+  p.is-editor-empty:first-child::before {
+    @apply text-muted-foreground h-0 pointer-events-none;
+    content: attr(data-placeholder);
+    float: left;
+  }
+
+  blockquote {
+    @apply border-l-3 border-border-strong pl-3 text-muted-foreground;
+  }
+
+  h1 {
+    @apply text-4xl;
+  }
+  h2 {
+    @apply text-3xl;
+  }
+  h3 {
+    @apply text-2xl;
+  }
+  h4 {
+    @apply text-xl;
+  }
+
+  li p {
+    display: inline;
+  }
+
+  ul,
+  ol {
+    padding-left: 1.5em;
+    list-style-type: disc;
+  }
+
+  ul ul {
+    list-style-type: circle;
+  }
+  ul ul ul {
+    list-style-type: square;
+  }
+}
+</style>
