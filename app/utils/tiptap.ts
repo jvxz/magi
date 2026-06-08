22995cb9334f3af3e0ccbff6651ba9d1bf07166e
Jamie <hi@jamie.to>
2026-06-08 17:38:44 -0400

    feat: add tiptap utils

---
+++ app/utils/tiptap.ts
@@ -0,0 +1,42 @@
+import type { MentionNodeAttrs } from '@tiptap/extension-mention'
+import type { Node } from '@tiptap/pm/model'
+
+export function nodeToFormattedBody(node: Node) {
+  if (node.isText) return MARKED_INSTANCE.parseInline(node.text ?? '') as string
+
+  console.log('node.type.name: ', node.type.name)
+  switch (node.type.name) {
+    case 'hardBreak':
+      return '<br/>'
+    case 'mention':
+      return mentionToHtml(node)
+    case 'emoji':
+      return emojiToHtml(node)
+    case 'blockquote':
+      return blockquoteToHtml(node)
+    default:
+      return childrenToHtml(node)
+  }
+}
+
+function mentionToHtml(node: Node) {
+  const attrs = node.attrs as MentionNodeAttrs
+  return `<a href="https://matrix.to/#/${encodeURIComponent(String(attrs.id))}">@${attrs.label}</a>`
+}
+
+function emojiToHtml(node: Node) {
+  const attrs = node.attrs as { unicode: string }
+  return `<span>${attrs.unicode}</span>`
+}
+
+function blockquoteToHtml(node: Node) {
+  return `<blockquote>${childrenToHtml(node)}</blockquote>`
+}
+
+function childrenToHtml(node: Node) {
+  let output = ''
+  node.forEach(c => {
+    output += nodeToFormattedBody(c)
+  })
+  return output
+}
