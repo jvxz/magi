db0013fe2c476780d5fa0198373a52cb09a0417e
Jamie <hi@jamie.to>
2026-06-08 20:17:53 -0400

    fix: conditionally use DOMPurify hooks rather than registering in module scope

--- app/utils/matrix/messaging.ts
+++ app/utils/matrix/messaging.ts
@@ -1,10 +1,10 @@
-import DOMPurify from 'dompurify'
+import DOMPurify, { type UponSanitizeAttributeHook } from 'dompurify'
 
 const DATA_MX_COLOR_RE = /^#[0-9a-f]{6}$/i
 
 type RestrictedTag = keyof typeof MATRIX.MESSAGING.ALLOWED_ATTRS_PER_TAG
 
-DOMPurify.addHook('uponSanitizeAttribute', (node, data) => {
+const sanitizeAttribute: UponSanitizeAttributeHook = (node, data) => {
   const tag = node.tagName.toLowerCase()
   const { attrName, attrValue } = data
 
@@ -19,13 +19,16 @@ DOMPurify.addHook('uponSanitizeAttribute', (node, data) => {
   else if ((attrName === 'data-mx-color' || attrName === 'data-mx-bg-color') && !DATA_MX_COLOR_RE.test(attrValue))
     data.keepAttr = false
   else if (attrName === 'src' && tag === 'img' && !attrValue.startsWith('mxc://')) data.keepAttr = false
-})
+}
 
 export function sanitizeFormattedBody(formattedBody: string) {
-  const sanitized = DOMPurify.sanitize(formattedBody, {
-    ALLOWED_ATTR: MATRIX.MESSAGING.ALLOWED_ATTRS as unknown as string[],
-    ALLOWED_TAGS: MATRIX.MESSAGING.ALLOWED_TAGS as unknown as string[],
-  })
-
-  return sanitized
+  DOMPurify.addHook('uponSanitizeAttribute', sanitizeAttribute)
+  try {
+    return DOMPurify.sanitize(formattedBody, {
+      ALLOWED_ATTR: MATRIX.MESSAGING.ALLOWED_ATTRS as unknown as string[],
+      ALLOWED_TAGS: MATRIX.MESSAGING.ALLOWED_TAGS as unknown as string[],
+    })
+  } finally {
+    DOMPurify.removeHook('uponSanitizeAttribute')
+  }
 }
