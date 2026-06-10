88110f5454722abb6e223b118e7c5386530b60d8
Jamie <hi@jamie.to>
2026-06-10 17:14:17 -0400

    feat: add caching to useMarked

--- app/composables/use-marked.ts
+++ app/composables/use-marked.ts
@@ -13,16 +13,25 @@ const cache = new QuickLRU<string, string>({ maxSize: 512 })
 export function useMarked(input: MaybeRefOrGetter<string | undefined>, options?: { inline?: boolean }) {
   return computed(() => {
     const inputValue = toValue(input)
-
     if (!inputValue) return
 
-    return cache.getOrInsert(
-      `${options?.inline ? 'i' : 'o'}:${inputValue}`,
-      DOMPurify.sanitize(
-        options?.inline
-          ? (md.parseInline(inputValue, { breaks: true }) as string)
-          : (md.parse(inputValue, { breaks: true }) as string),
-      ),
+    const key = getKey(inputValue)
+
+    const cached = cache.get(key)
+    if (cached) return cached
+
+    const content = DOMPurify.sanitize(
+      options?.inline
+        ? (md.parseInline(inputValue, { breaks: true }) as string)
+        : (md.parse(inputValue, { breaks: true }) as string),
     )
+
+    cache.set(key, content)
+
+    return content
   })
+
+  function getKey(input: string) {
+    return `${options?.inline ? 'i' : 'o'}:${input}`
+  }
 }
