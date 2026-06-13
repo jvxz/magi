59debee92167dbab6f6bec56bc4557c495738566
Jamie <hi@jamie.to>
2026-06-13 16:29:45 -0400

    feat: add recent rooms list component

---
+++ app/components/page/me/recent-rooms/list.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+const { sortedRecentRooms } = useRecentRooms()
+</script>
+
+<template>
+  <div class="p-3 flex flex-col gap-2.5">
+    <div v-for="recentRoom in sortedRecentRooms" :key="recentRoom.key">
+      <URoomShowcaseCardRoot :room="recentRoom.key" v-slot="{ room }">
+        <URoomShowcaseCardContent>
+          <URoomShowcaseCardTitle class="flex items-center">
+            <span>{{ room?.name }}</span>
+          </URoomShowcaseCardTitle>
+
+          <URoomShowcaseCardDescription>
+            <!-- <template v-if="recentRoom.parentSpaceId">
+              <MatrixAvatar :room="recentRoom.parentSpaceId" class="size-3" />
+
+              <UInlineSeparator />
+            </template> -->
+
+            <!-- <span>Last visited: <NuxtTime :datetime="recentRoom.lastVisited" /></span> -->
+
+            <UseRoom v-if="recentRoom.parentSpaceId" :room="recentRoom.parentSpaceId" v-slot="{ room }">
+              <span> {{ room?.name }}</span>
+            </UseRoom>
+          </URoomShowcaseCardDescription>
+        </URoomShowcaseCardContent>
+      </URoomShowcaseCardRoot>
+    </div>
+  </div>
+</template>
