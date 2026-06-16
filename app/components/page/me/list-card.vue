0143db14e1140ebe64d500c34bd65934b2b4908c
Jamie <hi@jamie.to>
2026-06-16 13:42:09 -0400

    feat: create dedicated list card component for home page lists

---
+++ app/components/page/me/list-card.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+defineProps<{ roomId: string; spaceId: string }>()
+</script>
+
+<template>
+  <URoomShowcaseCardRoot :room="roomId" v-slot="{ room }">
+    <URoomShowcaseCardContent>
+      <URoomShowcaseCardTitle class="flex items-center">
+        <span>{{ room?.name }}</span>
+      </URoomShowcaseCardTitle>
+
+      <URoomShowcaseCardDescription>
+        <UseRoom v-if="spaceId" :room="spaceId" v-slot="{ room }">
+          {{ room?.getJoinedMemberCount() }} members
+          <template v-if="getRoomTopic(room)">
+            <UInlineSeparator />
+            <span> {{ getRoomTopic(room) }}</span>
+          </template>
+        </UseRoom>
+      </URoomShowcaseCardDescription>
+    </URoomShowcaseCardContent>
+  </URoomShowcaseCardRoot>
+</template>
