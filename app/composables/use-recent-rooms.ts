2a47c7c2b5c50f549451cc5497a0c3af792d2f68
Jamie <hi@jamie.to>
2026-06-16 13:27:11 -0400

    refactor: use useScopedLocalStorage inside pinned/recent rooms composables

--- app/composables/use-recent-rooms.ts
+++ app/composables/use-recent-rooms.ts
@@ -1,16 +1,23 @@
 interface RecentRoomEntry {
   lastVisited: number
   isSpace?: boolean
-  parentSpaceId?: string
+  parentSpaceId: string
 }
 
 type RecentRoomsState = Map<string, RecentRoomEntry>
+type RecentRoomsBlacklistedState = Set<string>
 
 export const useRecentRooms = createGlobalState(() => {
-  const { self } = useSelf()
-  const recentRooms = useLocalStorage<RecentRoomsState>(() => `${self.value?.userId}:recentRooms`, new Map(), {
+  const recentRooms = useScopedLocalStorage<RecentRoomsState>('recentRooms', new Map(), {
     shallow: true,
   })
+  const recentRoomsBlacklisted = useScopedLocalStorage<RecentRoomsBlacklistedState>(
+    'recentRoomsBlacklisted',
+    new Set(),
+    {
+      shallow: true,
+    },
+  )
 
   const sortedRecentRooms = computed(() =>
     orderBy(
@@ -20,7 +27,9 @@ export const useRecentRooms = createGlobalState(() => {
     ),
   )
 
-  function bumpRecentRoom(room: MaybeRoomOrId, opts?: { isSpace?: boolean; parentSpaceId?: string }) {
+  function bumpRecentRoom(room: MaybeRoomOrId, opts: { isSpace?: boolean; parentSpaceId: string }) {
+    if (isRoomBlacklisted(room)) return
+
     recentRooms.value.set(resolveRoomId(room), {
       lastVisited: Date.now(),
       ...opts,
@@ -34,10 +43,28 @@ export const useRecentRooms = createGlobalState(() => {
     triggerRef(recentRooms)
   }
 
+  function blacklistRoom(room: MaybeRoomOrId) {
+    recentRoomsBlacklisted.value.add(resolveRoomId(room))
+    triggerRef(recentRoomsBlacklisted)
+    removeRecentRoom(room)
+  }
+
+  function unblacklistRoom(room: MaybeRoomOrId) {
+    recentRoomsBlacklisted.value.delete(resolveRoomId(room))
+    triggerRef(recentRoomsBlacklisted)
+  }
+
+  function isRoomBlacklisted(room: MaybeRoomOrId) {
+    return recentRoomsBlacklisted.value.has(resolveRoomId(room))
+  }
+
   return {
+    blacklistRoom,
     bumpRecentRoom,
+    isRoomBlacklisted,
     recentRooms,
     removeRecentRoom,
     sortedRecentRooms,
+    unblacklistRoom,
   }
 })
