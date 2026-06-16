7d3b91773e07d105c09a2b373b43ea606098b563
Jamie <hi@jamie.to>
2026-06-16 13:11:39 -0400

    feat: add collapsible components

---
+++ app/components/u/collapsible/content.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import type { CollapsibleContentEmits, CollapsibleContentProps } from 'reka-ui'
+
+import { useForwardPropsEmits } from 'reka-ui'
+
+const props = defineProps<CollapsibleContentProps & { class?: string }>()
+const emits = defineEmits<CollapsibleContentEmits>()
+
+const delegated = reactiveOmit(props, 'class')
+const forwarded = useForwardPropsEmits(delegated, emits)
+</script>
+
+<template>
+  <CollapsibleContent
+    v-bind="forwarded"
+    :class="cn('flex flex-col gap-2', $props.class)"
+    data-slot="collapsible-content"
+  >
+    <slot />
+  </CollapsibleContent>
+</template>
