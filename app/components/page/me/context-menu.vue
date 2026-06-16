82fac5ffff214c9e0787a86876127bb6f28583b4
Jamie <hi@jamie.to>
2026-06-16 13:42:16 -0400

    feat: create context menu component for home page

---
+++ app/components/page/me/context-menu.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+const { pinRoom, isRoomPinned, unpinRoom } = usePinnedRooms()
+const { blacklistRoom, removeRecentRoom } = useRecentRooms()
+const { payload } = useContextMenuRegion('homeRoom')
+
+const isPinned = computed(() => (payload.value?.roomId ? isRoomPinned(payload.value?.roomId) : false))
+
+function handlePin() {
+  if (!payload.value) return
+
+  if (!isPinned.value) {
+    pinRoom(payload.value.roomId, payload.value.spaceId)
+  } else unpinRoom(payload.value.roomId)
+}
+</script>
+
+<template>
+  <UContextMenuRegionContent name="homeRoom" v-if="payload?.roomId" align="start">
+    <UContextMenuItem as-child>
+      <NuxtLink
+        :to="{
+          name: 'space-room',
+          params: {
+            roomId: payload.roomId,
+            spaceId: payload.spaceId,
+          },
+        }"
+      >
+        <Icon name="tabler:door-enter" />
+        <span>Go to room</span>
+      </NuxtLink>
+    </UContextMenuItem>
+    <UContextMenuItem @select="handlePin">
+      <Icon :name="isPinned ? 'tabler:pinned-off' : 'tabler:pin'" />
+      <span>{{ isPinned ? 'Unpin' : 'Pin' }} room</span>
+    </UContextMenuItem>
+
+    <template v-if="payload.type === 'recent'">
+      <UContextMenuSeparator />
+
+      <UContextMenuItem @select="removeRecentRoom(payload.roomId)">
+        <Icon name="tabler:trash" />
+        <span>Remove from recents</span>
+      </UContextMenuItem>
+      <UContextMenuItem @select="blacklistRoom(payload.roomId)">
+        <Icon name="tabler:circle-x" />
+        <span>Never show in recents</span>
+      </UContextMenuItem>
+    </template>
+  </UContextMenuRegionContent>
+</template>
