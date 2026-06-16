c345f485ca7c11e9e49f549207d9d8158724fe0f
Jamie <hi@jamie.to>
2026-06-16 15:37:23 -0400

    feat: wrap home list card in link to room

--- app/components/page/me/list-card.vue
+++ app/components/page/me/list-card.vue
@@ -3,19 +3,34 @@ defineProps<{ roomId: string; spaceId: string }>()
 </script>
 
 <template>
-  <URoomShowcaseCardRoot v-slot="{ room }" :room="roomId">
-    <URoomShowcaseCardContent>
-      <URoomShowcaseCardTitle class="flex items-center">
-        <span>{{ room?.name }}</span>
-      </URoomShowcaseCardTitle>
+  <NuxtLink
+    class="group"
+    :to="{
+      name: 'space-room',
+      params: {
+        roomId,
+        spaceId,
+      },
+    }"
+  >
+    <URoomShowcaseCardRoot
+      v-slot="{ room }"
+      :room="roomId"
+      class="cursor-pointer group-data-[context-menu-open]:(border-border-strong bg-hover)"
+    >
+      <URoomShowcaseCardContent>
+        <URoomShowcaseCardTitle class="flex items-center">
+          <span>{{ room?.name }}</span>
+        </URoomShowcaseCardTitle>
 
-      <URoomShowcaseCardDescription>
-        {{ room?.getJoinedMemberCount() }} members
-        <template v-if="getRoomTopic(room)">
-          <UInlineSeparator />
-          <span> {{ getRoomTopic(room) }}</span>
-        </template>
-      </URoomShowcaseCardDescription>
-    </URoomShowcaseCardContent>
-  </URoomShowcaseCardRoot>
+        <URoomShowcaseCardDescription>
+          {{ room?.getJoinedMemberCount() }} members
+          <template v-if="getRoomTopic(room)">
+            <UInlineSeparator />
+            <span> {{ getRoomTopic(room) }}</span>
+          </template>
+        </URoomShowcaseCardDescription>
+      </URoomShowcaseCardContent>
+    </URoomShowcaseCardRoot>
+  </NuxtLink>
 </template>
