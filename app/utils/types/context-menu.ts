b16887aba4bf7c458c2881587f9bea500fcf02d5
Jamie <hi@jamie.to>
2026-06-16 13:31:36 -0400

    feat: add `homeRoom` context menu region

--- app/utils/types/context-menu.ts
+++ app/utils/types/context-menu.ts
@@ -9,6 +9,11 @@ export interface ContextMenuRegions {
     event: MatrixEvent
     roomId: string
   }
+  homeRoom: {
+    roomId: string
+    spaceId: string
+    type: 'recent' | 'pinned'
+  }
 }
 
 export type ContextMenuName = keyof ContextMenuRegions
