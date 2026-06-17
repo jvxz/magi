8fce8d98c1494023ecfe43a4f5cfe4e137adda30
Jamie <hi@jamie.to>
2026-06-17 12:06:25 -0400

    feat: add active tab indicator to aside

--- app/components/layout/app/aside.vue
+++ app/components/layout/app/aside.vue
@@ -3,7 +3,7 @@ const rooms = useRooms('space')
 </script>
 
 <template>
-  <aside class="shrink-0 w-20 top-0 sticky">
+  <aside class="shrink-0 w-20 top-0 sticky isolate">
     <LayoutAppAsideHome />
 
     <USeparator class="mx-auto mb-[2px] w-1/2!" />
@@ -12,6 +12,13 @@ const rooms = useRooms('space')
         <LazyLayoutAppAsideRoom v-for="room in rooms" :key="room.roomId" :room />
       </template>
       <LayoutAppAsidePublicRooms />
+    </div>
+
+    <div
+      aria-hidden="true"
+      class="pointer-events-none duration-100 flex items-center absolute position-anchor-active w-full anchor-top anchor-bottom ease-snappy z-1"
+    >
+      <div class="w-1 rounded-r bg-foreground h-3/4"></div>
     </div>
   </aside>
 </template>
