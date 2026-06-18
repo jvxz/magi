8b21012941d5c9172fa757dfc762ae179f975c34
Jamie <hi@jamie.to>
2026-06-18 15:07:01 -0400

    refactor: edit useCurrentSpaceId

--- app/composables/use-current-space-id.ts
+++ app/composables/use-current-space-id.ts
@@ -1,4 +1,4 @@
 export const useCurrentSpaceId = createSharedComposable(() => {
   const route = useRoute()
-  return computed(() => ('spaceId' in route.params ? route.params.spaceId : ''))
+  return computed(() => ('spaceId' in route.params ? route.params.spaceId : undefined))
 })
