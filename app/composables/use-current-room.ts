61581606f89be32a4953f9d1be8c7fc4135942ff
Jamie <hi@jamie.to>
2026-06-18 15:01:55 -0400

    feat: handle `directRoomId` param inside useCurrentRoom

--- app/composables/use-current-room.ts
+++ app/composables/use-current-room.ts
@@ -1,6 +1,12 @@
 export const useCurrentRoom = createGlobalState(() => {
   const route = useRoute()
-  const roomId = computed(() => ('roomId' in route.params ? route.params.roomId : undefined))
+  const roomId = computed(() =>
+    'roomId' in route.params
+      ? route.params.roomId
+      : 'directRoomId' in route.params
+        ? route.params.directRoomId
+        : undefined,
+  )
 
   const room = useRoom(roomId)
 
