c3d1b28537117bec6c7189dadc19e271ba7df7c5
Jamie <hi@jamie.to>
2026-06-18 15:07:15 -0400

    feat: enhance app layout page header component

--- app/components/layout/app/page-header.vue
+++ app/components/layout/app/page-header.vue
@@ -1,5 +1,18 @@
+<script lang="ts" setup>
+import type { PrimitiveProps } from 'reka-ui'
+
+const props = defineProps<PrimitiveProps & { class?: string }>()
+
+const delegated = reactiveOmit(props, 'class')
+</script>
+
 <template>
-  <div class="border-b bg-surface shrink-0 h-header-height w-full top-0 sticky z-20" v-bind="$attrs">
+  <Primitive
+    v-bind="delegated"
+    :class="
+      cn('border-b bg-surface shrink-0 h-header-height w-full top-0 sticky z-20 px-4 flex items-center', $props.class)
+    "
+  >
     <slot />
-  </div>
+  </Primitive>
 </template>
