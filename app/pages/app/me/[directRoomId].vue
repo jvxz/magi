3ab613f709e687c6b788272498bf239ff6d54f5e
Jamie <hi@jamie.to>
2026-06-19 14:07:36 -0400

    feat: add dropdown to header in direct room

--- app/pages/app/me/[directRoomId].vue
+++ app/pages/app/me/[directRoomId].vue
@@ -13,12 +13,33 @@ definePageMeta({
 
 const route = useRoute()
 const room = useRoom(() => route.params.directRoomId)
+
+const { self } = useSelf()
+const user = useUser(() => room.value?.getMembers().filter(m => m.userId !== self.value?.userId)[0]?.userId)
 </script>
 
 <template>
   <LayoutAppSlot name="page-header">
     <LayoutAppPageHeader>
-      <span>{{ room ? resolveRoomName(room) : 'Unknown room' }}</span>
+      <UProfilePopoverTrigger
+        v-if="user"
+        :user
+        as-child
+        :content-props="{
+          sideOffset: 12,
+        }"
+        :context="{
+          from: 'direct',
+        }"
+      >
+        <UButton
+          variant="link"
+          class="font-normal text-base gap-1 items-center context-menu-open:underline data-[state=open]:no-underline data-[popover-open]:underline!"
+        >
+          <Icon name="tabler:at" class="text-muted-foreground" />
+          <span> {{ room ? resolveRoomName(room) : 'Unknown room' }}</span>
+        </UButton>
+      </UProfilePopoverTrigger>
     </LayoutAppPageHeader>
   </LayoutAppSlot>
 
