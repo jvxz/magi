45aca60ae87fdb40ba6bb179f3f1017fd163e31c
Jamie <hi@jamie.to>
2026-06-22 20:03:30 -0400

    feat: add annotation components for dialogs

---
+++ app/components/u/alert-dialog/annotation.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import type { PrimitiveProps } from 'reka-ui'
+
+const props = withDefaults(defineProps<PrimitiveProps & { class?: string }>(), {
+  as: 'p',
+})
+
+const delegated = reactiveOmit(props, 'class')
+</script>
+
+<template>
+  <Primitive v-bind="delegated" :class="cn('text-muted-foreground text-sm mr-auto', props.class)">
+    <slot />
+  </Primitive>
+</template>
