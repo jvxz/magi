360849483991a9681e17243c7ce389cd12dee2f4
Jamie <hi@jamie.to>
2026-06-23 14:54:41 -0400

    chore: temporarily remove direct room header interactivity

--- app/pages/app/me/[directRoomId].vue
+++ app/pages/app/me/[directRoomId].vue
@@ -14,32 +14,14 @@ definePageMeta({
 const route = useRoute()
 const room = useRoom(() => route.params.directRoomId)
 
-const { self } = useSelf()
-const user = useUser(() => room.value?.getMembers().filter(m => m.userId !== self.value?.userId)[0]?.userId)
+// const { self } = useSelf()
+// const user = useUser(() => room.value?.getMembers().filter(m => m.userId !== self.value?.userId)[0]?.userId)
 </script>
 
 <template>
   <LayoutAppSlot name="page-header">
     <LayoutAppPageHeader>
-      <UProfilePopoverTrigger
-        v-if="user"
-        :user
-        as-child
-        :content-props="{
-          sideOffset: 12,
-        }"
-        :context="{
-          from: 'direct',
-        }"
-      >
-        <UButton
-          variant="link"
-          class="text-base font-normal gap-1 items-center context-menu-open:underline data-[state=open]:no-underline data-[popover-open]:underline!"
-        >
-          <Icon name="tabler:at" class="text-muted-foreground" />
-          <span> {{ room ? resolveRoomName(room) : 'Unknown room' }}</span>
-        </UButton>
-      </UProfilePopoverTrigger>
+      <span> {{ room ? resolveRoomName(room) : 'Unknown room' }}</span>
     </LayoutAppPageHeader>
   </LayoutAppSlot>
 
