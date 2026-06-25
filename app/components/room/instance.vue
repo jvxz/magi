65fd221e4ddb9569dbd5754d8f08f5486ecc9099
Jamie <hi@jamie.to>
2026-06-25 14:11:28 -0400

    feat: integrate preview into room instance

--- app/components/room/instance.vue
+++ app/components/room/instance.vue
@@ -2,18 +2,26 @@
 const props = withDefaults(defineProps<{ withMembersList?: boolean; room: MaybeRoomOrId | undefined }>(), {
   withMembersList: true,
 })
-
+const roomId = useResolveRoomId(() => props.room)
 const room = useRoom(() => props.room)
+const { data: summary } = useRoomSummary(() => props.room)
+const isJoined = useRoomIsJoined(room)
 </script>
 
 <template>
-  <div v-if="room" class="flex flex-1 size-full">
-    <div class="flex flex-col size-full relative">
-      <RoomEventList :room />
-      <RoomInput />
+  <RoomInstancePreview v-if="!room && !isJoined && roomId" :summary :room-id />
+
+  <template v-else-if="room">
+    <div v-if="isJoined" class="flex flex-1 size-full">
+      <div class="flex flex-col size-full relative">
+        <RoomEventList :room />
+        <RoomInput />
+      </div>
+
+      <RoomMembersList v-if="withMembersList" />
+      <RoomEventReactionsViewer />
     </div>
 
-    <RoomMembersList v-if="withMembersList" />
-    <RoomEventReactionsViewer />
-  </div>
+    <div v-else>your not joined</div>
+  </template>
 </template>
