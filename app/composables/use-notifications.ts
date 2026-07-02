6fa2a80127dc482a1678802e991914d5f287bd22
Jamie <hi@jamie.to>
2026-07-02 12:46:38 -0400

    feat: enhance `useNotifications` composable

--- app/composables/use-notifications.ts
+++ app/composables/use-notifications.ts
@@ -1,24 +1,25 @@
-export interface Notification extends Pick<Toast, 'title' | 'description' | 'actions' | 'icon' | 'variant'> {
+export interface AppNotification extends Pick<Toast, 'title' | 'description' | 'actions' | 'icon' | 'variant'> {
   id: string
+  timestamp: number
 }
 
-interface NotificationOptions {
+interface AppNotificationOptions {
   emitToast?: boolean
   persist?: boolean
 }
 
 export const useNotifications = createSharedComposable(() => {
-  const notificationsMap = useScopedLocalStorage<Map<Notification['id'], Notification>>('notis', new Map(), {
+  const notificationsMap = useScopedLocalStorage<Map<AppNotification['id'], AppNotification>>('notis', new Map(), {
     shallow: true,
   })
   const toast = useToast()
 
-  const notify = (notification: Omit<Notification, 'id'>, opts?: Partial<NotificationOptions>) => {
+  const notify = (notification: Omit<AppNotification, 'id' | 'timestamp'>, opts?: Partial<AppNotificationOptions>) => {
     const { emitToast = true, persist = true } = opts ?? {}
 
     if (persist) {
       const id = crypto.randomUUID()
-      notificationsMap.value.set(id, { ...notification, id })
+      notificationsMap.value.set(id, { ...notification, id, timestamp: Temporal.Now.instant().epochMilliseconds })
       triggerRef(notificationsMap)
     }
 
@@ -27,15 +28,18 @@ export const useNotifications = createSharedComposable(() => {
     }
   }
 
-  const dismiss = (id: Notification['id']) => {
+  const dismiss = (id: AppNotification['id']) => {
     notificationsMap.value.delete(id)
     triggerRef(notificationsMap)
   }
+
+  const dismissAll = () => (notificationsMap.value = new Map())
 
   const notifications = computed(() => [...notificationsMap.value.values()])
 
   return {
     dismiss,
+    dismissAll,
     notifications,
     notify,
   }
