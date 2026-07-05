5b2bf2e01ec7629890317126de5c777db071a7c9
Jamie <hi@jamie.to>
2026-07-05 15:02:53 -0400

    feat: add toast description + title components

---
+++ app/components/u/toast/description.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import type { ToastDescriptionProps } from 'reka-ui'
+
+import type { AlertDescriptionProps } from '../alert/alert-description.vue'
+
+defineProps<ToastDescriptionProps & AlertDescriptionProps & { class?: string }>()
+</script>
+
+<template>
+  <ToastDescription v-bind="$props" data-slot="toast-title" as-child>
+    <UAlertDescription>
+      <slot />
+    </UAlertDescription>
+  </ToastDescription>
+</template>
---
+++ app/components/u/toast/title.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import type { ToastTitleProps } from 'reka-ui'
+
+import type { AlertTitleProps } from '../alert/alert-title.vue'
+
+const props = defineProps<ToastTitleProps & AlertTitleProps & { class?: string }>()
+
+const delegated = reactiveOmit(props, 'class')
+</script>
+
+<template>
+  <ToastTitle v-bind="delegated" data-slot="toast-title" as-child>
+    <UAlertTitle :class="props.class">
+      <slot />
+    </UAlertTitle>
+  </ToastTitle>
+</template>
