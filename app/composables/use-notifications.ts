c581d62911d5e1fdf41b04a63dd8139b60e11999
Jamie <hi@jamie.to>
2026-07-05 14:46:14 -0400

    feat: enhance useNotifications with predicate fn + improved types

--- app/composables/use-notifications.ts
+++ app/composables/use-notifications.ts
@@ -1,11 +1,7 @@
-export interface AppNotification extends Pick<Toast, 'title' | 'description' | 'actions' | 'icon' | 'variant'> {
-  id: string
-  timestamp: number
-}
-
 interface AppNotificationOptions {
   emitToast?: boolean
   persist?: boolean
+  variant?: AlertVariants
 }
 
 export const useNotifications = createSharedComposable(() => {
@@ -14,21 +10,51 @@ export const useNotifications = createSharedComposable(() => {
   })
   const toast = useToast()
 
-  const notify = (notification: Omit<AppNotification, 'id' | 'timestamp'>, opts?: Partial<AppNotificationOptions>) => {
+  const notify = <T extends AppNotificationName>(
+    key: T,
+    notification: Omit<AppNotification<T>, 'key' | 'id' | 'timestamp'> & { id?: string },
+    opts?: Partial<AppNotificationOptions>,
+  ) => {
     const { emitToast = true, persist = true } = opts ?? {}
 
+    const id = notification.id ?? crypto.randomUUID()
     if (persist) {
-      const id = crypto.randomUUID()
-      notificationsMap.value.set(id, { ...notification, id, timestamp: Temporal.Now.instant().epochMilliseconds })
+      notificationsMap.value.set(id, {
+        ...notification,
+        id,
+        key,
+        timestamp: Temporal.Now.instant().epochMilliseconds,
+      })
       triggerRef(notificationsMap)
     }
 
     if (emitToast) {
-      toast.add(notification)
+      toast.add({ ...notification, id, key, variant: opts?.variant })
     }
   }
 
+  const notifyError = (error: Error | ErrorShape, title: string) => {
+    const shape = isError(error) ? parseError(error) : error
+    notify(
+      'error',
+      {
+        payload: {
+          description: `${shape.title}: ${shape.message}`,
+          raw: shape.raw,
+          title,
+        },
+      },
+      {
+        persist: false,
+        variant: 'danger',
+      },
+    )
+  }
+
   const dismiss = (id: AppNotification['id']) => {
+    const noti = notificationsMap.value.get(id)
+    if (!noti) return
+
     notificationsMap.value.delete(id)
     triggerRef(notificationsMap)
   }
@@ -41,6 +67,15 @@ export const useNotifications = createSharedComposable(() => {
     dismiss,
     dismissAll,
     notifications,
+    notificationsMap,
     notify,
+    notifyError,
   }
 })
+
+export function isNotificationType<T extends AppNotificationName>(
+  notification: AppNotification,
+  key: T,
+): notification is AppNotification<T> {
+  return notification.key === key
+}
