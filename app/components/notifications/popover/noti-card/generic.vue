8f8d1d56c35b356beed2a5c4d744982f5a510467
Jamie <hi@jamie.to>
2026-07-05 15:06:06 -0400

    feat: add notification type-specific card components

---
+++ app/components/notifications/popover/noti-card/generic.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+defineProps<{ notification: AppNotification<'generic'> }>()
+</script>
+
+<template>
+  <NotificationsPopoverNotiCard
+    :description="notification.payload.description"
+    :title="notification.payload.title"
+    :id="notification.id"
+  />
+</template>
