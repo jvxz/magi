c43450b7e082dfec06706b4a2e5fe34b6b8e5ec2
Jamie <hi@jamie.to>
2026-07-05 14:16:20 -0400

    refactor: rework `parseMatrixError` to be a generic `parseError`

--- app/utils/matrix/parse-matrix-error.ts
+++ app/utils/matrix/parse-matrix-error.ts
@@ -1,13 +1,39 @@
 import { MatrixError, TokenRefreshError } from 'matrix-js-sdk'
 
-export function parseMatrixError(error: unknown, opts?: { fallbackMessage?: string }) {
+export interface ErrorShape {
+  code?: string
+  message: string
+  title?: string
+  raw: string
+}
+
+export function parseError(error: unknown, opts?: { fallbackMessage?: string }): ErrorShape {
   const fallbackMessage = opts?.fallbackMessage ?? 'Unknown error'
 
-  if (error instanceof $Error) return error.message
+  if (error instanceof $Error)
+    return {
+      message: error.message,
+      raw: JSON.stringify(error),
+      title: error.name,
+    }
 
-  if (error instanceof MatrixError) return error.data.error ?? fallbackMessage
+  if (error instanceof MatrixError)
+    return {
+      code: error.errcode,
+      message: error.data.error ?? error.message,
+      raw: JSON.stringify(error.data),
+      title: error.data.errcode,
+    }
 
-  if (error instanceof TokenRefreshError) return error.message ?? fallbackMessage
+  if (error instanceof TokenRefreshError)
+    return {
+      message: error.message,
+      raw: JSON.stringify(error),
+      title: error.name,
+    }
 
-  return fallbackMessage
+  return {
+    message: fallbackMessage,
+    raw: JSON.stringify(error),
+  }
 }
