a195139879eb03472e4dfefcf901ac5a549a9f23
Jamie <hi@jamie.to>
2026-07-05 14:46:38 -0400

    chore: add EmitsToProps type

--- app/utils/types.ts
+++ app/utils/types.ts
@@ -10,8 +10,6 @@ export type Prettify<T> = {
 
 export type MaybeReadonlySet<T> = ReadonlySet<T> | Set<T>
 
-export type AsideDisplayMode = keyof typeof ASIDE_DISPLAY_MODES
-
 export type MaybeRefsOrGetters<T extends object> = Prettify<{
   [K in keyof T]: MaybeRefOrGetter<UnwrapRef<T[K]>>
 }>
@@ -21,3 +19,10 @@ export type DeepMaybeRefsOrGetters<T extends object> = Prettify<{
     ? MaybeRefsOrGetters<UnwrapRef<T[K]>>
     : MaybeRefOrGetter<UnwrapRef<T[K]>>
 }>
+
+// https://github.com/nuxt/ui/blob/dde09d06486e68b1b4dd4538f91fefd08a3d7548/src/runtime/types/utils.ts#L125-L129
+export type EmitsToProps<T> = {
+  [K in keyof T as `on${Capitalize<string & K>}`]: T[K] extends [...args: infer Args] ? (...args: Args) => void : never
+}
+
+export type AsideDisplayMode = keyof typeof ASIDE_DISPLAY_MODES
