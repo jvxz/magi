3510247efbfdf83b0dd0ee16ae23d66b363f3b03
Jamie <hi@jamie.to>
2026-07-05 16:27:05 -0400

    feat: add self ref updates based on matrix hooks

--- app/composables/use-self.ts
+++ app/composables/use-self.ts
@@ -1,7 +1,10 @@
 export const useSelf = createGlobalState(() => {
   const { client } = useMatrixClient()
+  const { onUserProfile, onSync } = useMatrixHooks()
 
-  const self = useUser(() => client.value.getSafeUserId())
+  const self = useUser(() => client.value.getUserId())
+  onUserProfile(self.trigger)
+  onSync(self.trigger)
 
   return { self }
 })
