81fc9bb263972028d9dd5e3608ad1a12a80dbabd
Jamie <hi@jamie.to>
2026-07-07 14:23:22 -0400

    perf: use new usePaginatedScroll composable in event list

--- app/components/room/event-list.vue
+++ app/components/room/event-list.vue
@@ -66,6 +66,7 @@ const groupedEvents = useEventGrouping({
             :style="isTestMode() ? { height: `${(event as any)._size}px` } : undefined"
           >
             <RoomEventGeneric :event :grouped="groupedEvents.grouped[idx] !== false" :room />
+            <!-- <RoomEventGeneric :grouped="false" :event :room /> -->
           </div>
           <div data-ignore class="h-12" />
         </div>
