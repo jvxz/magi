f9685c8ea6f5bd5dafc5003b70aeb696dd50211c
Jamie <hi@jamie.to>
2026-07-09 13:01:48 -0400

    feat: enhance me page aside buttons component

--- app/components/page/me/aside-buttons.vue
+++ app/components/page/me/aside-buttons.vue
@@ -1,12 +1,4 @@
 <script lang="ts" setup>
-const route = useRoute()
-const toggleValue = shallowRef('home')
-const toggle = computed({
-  // fallback to "recent rooms" button if no param is present
-  get: () => ('directRoomId' in route.params ? route.params.directRoomId : 'home'),
-  set: (v: string) => (toggleValue.value = v),
-})
-
 const displayMode = useScopedLocalStorage<AsideDisplayMode>('asideDisplayMode', 'all')
 provide('displayMode', displayMode)
 
@@ -17,8 +9,10 @@ const rooms = useRooms(
         return isDirect(room, axes) || (isGroup(room, axes) && isOrphan(room, axes))
       case 'direct':
         return isDirect(room, axes)
+      case 'loose':
+        return isGroup(room, axes) && isOrphan(room, axes) && getRoomParentSpaceIds(room).length > 0
       case 'orphan':
-        return isGroup(room, axes) && isOrphan(room, axes)
+        return isGroup(room, axes) && isOrphan(room, axes) && getRoomParentSpaceIds(room).length === 0
     }
   },
   {
@@ -28,34 +22,39 @@ const rooms = useRooms(
 </script>
 
 <template>
-  <UAsideListRoot as-child>
-    <UToggleGroupRoot v-model:model-value="toggle">
-      <UToggleGroupItem value="home" class="flex w-full items-center" as-child>
-        <NuxtLink to="/app/me/home">
-          <LazyIcon name="tabler:home" class="size-1lh!" />
-          <span class="font-medium">Home</span>
-        </NuxtLink>
-      </UToggleGroupItem>
-      <UToggleGroupItem value="invites" class="flex w-full items-center" as-child>
-        <NuxtLink to="/app/me/home">
-          <LazyIcon name="tabler:users-plus" class="size-1lh!" />
-          <span class="font-medium">Invites</span>
-        </NuxtLink>
-      </UToggleGroupItem>
+  <UAsideListRoot>
+    <UAsideListButton
+      class="justify-start items-center flex w-full router-link-active:(bg-selected hover:bg-selected text-foreground)"
+      as-child
+    >
+      <NuxtLink to="/app/me/home">
+        <LazyIcon name="tabler:home" class="size-1lh!" />
+        <span class="font-medium">Home</span>
+      </NuxtLink>
+    </UAsideListButton>
 
-      <div class="w-full space-y-2">
-        <USeparator class="my-2.5" />
+    <UAsideListButton
+      class="justify-start items-center flex w-full router-link-active:(bg-selected hover:bg-selected text-foreground)"
+      as-child
+    >
+      <NuxtLink to="/app/me/invites">
+        <LazyIcon name="tabler:inbox" class="size-1lh!" />
+        <span class="font-medium">Invites</span>
+      </NuxtLink>
+    </UAsideListButton>
 
-        <PageMeAsideButtonsHeader />
-      </div>
+    <div class="w-full space-y-2">
+      <USeparator class="my-2.5" />
 
-      <div class="flex flex-col gap-1 w-full">
-        <UContextMenuRegionRoot name="directRoom">
-          <PageMeAsideButtonsDirectRoom v-for="room in rooms" :key="room.roomId" :room-id="room.roomId" />
+      <PageMeAsideButtonsHeader />
+    </div>
 
-          <PageMeAsideButtonsContextMenuContent />
-        </UContextMenuRegionRoot>
-      </div>
-    </UToggleGroupRoot>
+    <div class="flex flex-col gap-1 w-full">
+      <UContextMenuRegionRoot name="directRoom">
+        <PageMeAsideButtonsDirectRoom v-for="room in rooms" :key="room.roomId" :room-id="room.roomId" />
+
+        <PageMeAsideButtonsContextMenuContent />
+      </UContextMenuRegionRoot>
+    </div>
   </UAsideListRoot>
 </template>
