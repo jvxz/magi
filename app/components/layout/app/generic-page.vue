968bbbbf99223a55c905380772229f9a00f8d123
Jamie <hi@jamie.to>
2026-07-09 12:59:53 -0400

    feat: enhance app layout generic page component

--- app/components/layout/app/generic-page.vue
+++ app/components/layout/app/generic-page.vue
@@ -1,19 +1,24 @@
 <script lang="ts" setup>
 import type { PrimitiveProps } from 'reka-ui'
 
-defineProps<
+const props = defineProps<
   PrimitiveProps & {
     classes?: DefineClasses<'container' | 'shell'>
+    class?: string
   }
 >()
+
+const delegated = reactiveOmit(props, ['class', 'classes'])
 </script>
 
 <template>
   <Primitive
-    v-bind="$props"
-    :class="cn('py-page-y-padding h-full relative overflow-y-scroll scrollbar-gutter-stable', $props.classes?.shell)"
+    v-bind="delegated"
+    :class="cn('py-page-y-padding h-full relative overflow-y-scroll scrollbar-gutter-stable', props.classes?.shell)"
   >
-    <div :class="cn('mx-auto container max-w-screen-xl space-y-lg', $props.classes?.container)">
+    <div
+      :class="cn('mx-auto container max-w-generic-page-max-width space-y-lg', props.class, props.classes?.container)"
+    >
       <slot />
     </div>
   </Primitive>
