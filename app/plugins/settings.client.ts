add17ce3c31d8e6ec497f33e0a79e384a714c804
Jamie <hi@jamie.to>
2026-07-12 15:26:55 -0400

    feat: scope settings local storage + merge defaults on app load (#121)

--- app/plugins/settings.client.ts
+++ app/plugins/settings.client.ts
@@ -1,7 +1,10 @@
 export default defineNuxtPlugin({
   parallel: true,
   setup: () => {
-    const settings = useLocalStorage<Settings>('settings', DEFAULT_SETTINGS)
+    const settings = useScopedLocalStorage<Settings>('settings', DEFAULT_SETTINGS)
+
+    // merge once on app load
+    settings.value = merge(cloneDeep(DEFAULT_SETTINGS), settings.value)
 
     return {
       provide: {
