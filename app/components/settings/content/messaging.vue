883702335aa0ed0c87f2ca33bea8ee1db9f6a3de
Jamie <hi@jamie.to>
2026-07-13 12:29:19 -0400

    feat: optionally disable typing events (#123)

---
+++ app/components/settings/content/messaging.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const settings = useSettings()
+</script>
+
+<template>
+  <SettingsContentLayout>
+    <SettingsFormSwitch
+      v-model:model-value="settings.messaging.typingEvents"
+      :label="SETTINGS_ITEM_METADATA.messaging.typingEvents.title"
+      :description="SETTINGS_ITEM_METADATA.messaging.typingEvents.description"
+    >
+    </SettingsFormSwitch>
+  </SettingsContentLayout>
+</template>
