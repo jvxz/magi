3e11bc062e9e3bc3e756b17765bae32a449cb7e6
Jamie <hi@jamie.to>
2026-07-13 15:07:26 -0400

    feat: add date separator room event component

---
+++ app/components/room/event/date-separator.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import type { MatrixEvent } from 'matrix-js-sdk'
+
+defineProps<{ event: MatrixEvent }>()
+</script>
+
+<template>
+  <div class="mt-4.25 px-6 flex items-center">
+    <USeparator class="w-full flex-1" />
+
+    <NuxtTime
+      :datetime="event.getTs()"
+      v-bind="PROPS__NUXT_TIME_SHORT_DATE"
+      class="shrink-0 px-2 text-muted-foreground text-sm tabular-nums"
+    />
+
+    <USeparator class="w-full flex-1" />
+  </div>
+</template>
