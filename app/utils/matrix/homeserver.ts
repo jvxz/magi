4327a5285c1f4045858f20e1ac0204479cbfa58e
Jamie <hi@jamie.to>
2026-07-16 12:53:34 -0400

    feat: enhance homeserver utils

--- app/utils/matrix/homeserver.ts
+++ app/utils/matrix/homeserver.ts
@@ -1,4 +1,4 @@
-import type { ClientConfig, LoginFlow } from 'matrix-js-sdk'
+import type { ClientConfig, ILoginFlowsResponse, LoginFlow } from 'matrix-js-sdk'
 
 import { AutoDiscovery, AutoDiscoveryAction, createClient } from 'matrix-js-sdk'
 
@@ -13,12 +13,40 @@ export function isHomeserverValid(config: ClientConfig) {
 
 export async function getLoginFlows(homeserver: string) {
   const config = await getHomeserverConfig(homeserver)
+
+  const isValid = isHomeserverValid(config)
+  if (!isValid) return throwErr()
+
   const client = createClient({ baseUrl: config['m.homeserver'].base_url ?? homeserver })
-  return client.loginFlows()
+
+  const [loginFlowsError, loginFlows] = await attemptAsync<ILoginFlowsResponse, Error>(() => client.loginFlows())
+  if (loginFlowsError) {
+    if (loginFlowsError instanceof TypeError && loginFlowsError.message.includes('URL')) return throwErr()
+    throw loginFlowsError
+  }
+
+  return loginFlows
+
+  function throwErr() {
+    throw new $Error({
+      code: ErrorCode.InvalidUrl,
+      message:
+        'Failed to make login request with provided homeserver URL. Please ensure it is correct and no typos were made.',
+      title: 'Failed to fetch',
+    })
+  }
+}
+
+export function getAuthFlow(loginFlows: LoginFlow[], targetFlows: string[]) {
+  return loginFlows.find(f => targetFlows.includes(f.type))
+}
+
+export function getPwFlow(loginFlows: LoginFlow[]) {
+  return getAuthFlow(loginFlows, ['m.login.password'])
 }
 
 export function getSSOFlow(loginFlows: LoginFlow[]) {
-  return loginFlows.find(f => ['m.login.sso', 'm.login.cas'].includes(f.type))
+  return getAuthFlow(loginFlows, ['m.login.sso', 'm.login.cas'])
 }
 
 export async function resolveHomeserverBaseUrl(baseUrl: string): Promise<string> {
