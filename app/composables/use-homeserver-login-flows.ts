73840f226b6fac11e6c9eb171a16c364afb69368
Jamie <hi@jamie.to>
2026-07-16 12:53:18 -0400

    feat: add retry login in `useHomeserverLoginFlows`

--- app/composables/use-homeserver-login-flows.ts
+++ app/composables/use-homeserver-login-flows.ts
@@ -4,6 +4,11 @@ export const useHomeserverLoginFlows = (homeserver: MaybeRefOrGetter<string | un
   const q = useQuery({
     queryFn: () => (homeserverRef.value ? (getLoginFlows(homeserverRef.value) ?? null) : null),
     queryKey: $qk.homeserverLoginFlows(homeserverRef),
+    retry: (n, e) => {
+      const parsed = parseError(e)
+      if (parsed.code === ErrorCode.InvalidUrl || n > 2) return false
+      return true
+    },
   })
 
   return q
