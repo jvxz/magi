9e558963065b338d68363ca3ad55c85478d2d997
Jamie <hi@jamie.to>
2026-07-17 20:45:54 -0400

    fix: early return on ssr for sso cleanup middleware (#129)

--- app/middleware/sso-cleanup.global.ts
+++ app/middleware/sso-cleanup.global.ts
@@ -1,4 +1,5 @@
 export default defineNuxtRouteMiddleware(async to => {
+  if (import.meta.server) return
   if (to.name !== 'login-sso') {
     // no need to await this, just cleanup
     void idb.remove(SSO_BASE_URL_KEY)
