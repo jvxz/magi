a5e527435ebf2346ab088fdbca00820c378d7392
Jamie <hi@jamie.to>
2026-07-20 13:56:25 -0400

    refactor: replace existing global timestamp tooltip logic with generic tooltip region logic

---
+++ app/components/room/event/list-providers.vue
@@ -0,0 +1,24 @@
+<template>
+  <UTooltipRegionRoot :delay-duration="500" name="timestamp">
+    <UContextMenuRegionRoot name="event">
+      <UContextMenuRegionRoot name="member">
+        <slot />
+
+        <!-- member ctx menu -->
+        <UContextMenuRegionContent v-slot="{ payload }" name="member">
+          <RoomContextMenuMember v-if="payload" v-bind="payload" />
+        </UContextMenuRegionContent>
+      </UContextMenuRegionRoot>
+
+      <!-- event ctx menu -->
+      <UContextMenuRegionContent v-slot="{ payload }" name="event">
+        <RoomContextMenuEvent v-if="payload" v-bind="payload" />
+      </UContextMenuRegionContent>
+    </UContextMenuRegionRoot>
+
+    <!-- timestamp tooltip -->
+    <UTooltipRegionContent name="timestamp" v-slot="{ payload }">
+      <NuxtTime v-if="payload" :datetime="payload?.datetime" v-bind="PROPS__NUXT_TIME_FULL" />
+    </UTooltipRegionContent>
+  </UTooltipRegionRoot>
+</template>
