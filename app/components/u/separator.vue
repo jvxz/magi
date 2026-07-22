89ac5549e37121357633781f12b6ca6433096358
Jamie <hi@jamie.to>
2026-07-22 13:42:23 -0400

    feat: enhance separator component

--- app/components/u/separator.vue
+++ app/components/u/separator.vue
@@ -1,24 +1,30 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import type { SeparatorProps } from 'reka-ui'
 import type { HTMLAttributes } from 'vue'
 
-const props = withDefaults(defineProps<SeparatorProps & { class?: HTMLAttributes['class'] }>(), {
-  decorative: true,
-  orientation: 'horizontal',
-})
+import { useForwardPropsEmits } from 'reka-ui'
 
-const delegatedProps = reactiveOmit(props, 'class')
+export interface USeparatorProps extends SeparatorProps {
+  class?: HTMLAttributes['class']
+}
+
+const props = defineProps<USeparatorProps>()
+
+const delegated = reactiveOmit(props, 'class')
+const forwarded = useForwardPropsEmits(delegated)
 </script>
 
 <template>
   <Separator
-    data-slot="separator"
-    v-bind="delegatedProps"
+    v-bind="forwarded"
     :class="
       cn(
         'shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px',
         props.class,
       )
     "
-  />
+    data-slot="separator"
+  >
+    <slot />
+  </Separator>
 </template>
