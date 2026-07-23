25bc12e333cfa622bf6bbfba27304985981bf753
Jamie <hi@jamie.to>
2026-07-23 16:51:54 -0400

    feat: enhance explore page styles

--- app/components/page/explore/room.vue
+++ app/components/page/explore/room.vue
@@ -5,47 +5,36 @@ const props = defineProps<{
   room: IPublicRoomsChunkRoom | undefined
 }>()
 
-const { client } = useMatrixClient()
-
-const src = computed(() =>
-  props.room && props.room.avatar_url
-    ? (mxcToHttps(props.room.avatar_url, {
-        allowDirectLinks: false,
-        allowRedirects: true,
-        baseUrl: client.value.getHomeserverUrl(),
-        height: 32,
-        resizeMethod: 'scale',
-        useAuthentication: true,
-        width: 32,
-      }) ?? '')
-    : '',
-)
+const src = useResolveAvatarUrl(() => props.room?.avatar_url)
 </script>
 
 <template>
-  <div class="rounded-lg w-full relative overflow-hidden" :class="!room && 'border'">
+  <div class="rounded-lg relative overflow-clip p-0 border-none" :class="!room && 'border'">
     <div class="h-full inset-0 absolute">
-      <img
-        v-if="src"
+      <MatrixAvatar
+        v-if="room"
         :size="32"
         :alt="room?.name"
         :src
         class="size-full scale-120 object-cover blur-xl -m-px -translate-y-1/3"
       />
-      <div v-else class="bg-muted size-full"></div>
+      <div v-else class="bg-surface-top size-full border border-border border-b-none rounded-t-lg"></div>
     </div>
 
-    <div class="mt-2/5 p-4 pt-1/8 border border-t-0 rounded-b-lg bg-surface flex flex-col gap-2 relative z-10 isolate">
-      <AvatarRoot
-        class="border-5 border-surface rounded-2xl bg-surface flex size-16 items-center inset-0 left-4 justify-center absolute z-10 overflow-hidden -top-1/8"
-      >
-        <template v-if="room">
-          <AvatarImage v-if="src" :src="src" />
-          <AvatarFallback class="text-sm text-foreground font-medium">
-            {{ room.name ? room.name.slice(0, 2) : room.room_id.slice(0, 2) }}
-          </AvatarFallback>
-        </template>
-      </AvatarRoot>
+    <UCard
+      variant="raised"
+      class="mt-2/5 p-4 pt-1/8 border border-t-0 rounded-b-lg rounded-t-0 flex flex-col gap-2 relative z-10 isolate"
+    >
+      <MatrixAvatar
+        v-if="room"
+        :src
+        class="border-5 border-surface-raised rounded-2xl bg-surface-raised flex size-16 items-center inset-0 left-4 justify-center absolute z-10 overflow-hidden -top-1/8"
+        square
+      />
+      <div
+        v-else
+        class="border-5 border-surface-raised rounded-2xl bg-surface-raised flex size-16 items-center inset-0 left-4 justify-center absolute z-10 overflow-hidden -top-1/8"
+      ></div>
 
       <h3 :title="room && room.name" class="font-medium h-1lh w-full truncate">
         {{ room && room.name }}
@@ -63,6 +52,6 @@ const src = computed(() =>
           {{ room ? 'members' : '' }}
         </p>
       </div>
-    </div>
+    </UCard>
   </div>
 </template>
