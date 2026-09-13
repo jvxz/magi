6ec990490f53d49a7535282de9cfd4f0f4757fa7
Jamie <hi@jamie.to>
2026-09-13 15:04:46 -0400

    feat: enhance settings default merge logic

--- app/plugins/settings.client.ts
+++ app/plugins/settings.client.ts
@@ -2,10 +2,9 @@ export default defineNuxtPlugin({
   name: 'settings',
   parallel: true,
   setup: () => {
-    const settings = useScopedLocalStorage<Settings>('settings', DEFAULT_SETTINGS)
-
-    // merge once on app load
-    settings.value = merge(cloneDeep(DEFAULT_SETTINGS), settings.value)
+    const settings = useScopedLocalStorage<Settings>('settings', DEFAULT_SETTINGS, {
+      mergeDefaults: (stored, defaults) => merge(cloneDeep(defaults), stored),
+    })
 
     return {
       provide: {
